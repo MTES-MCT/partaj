ad50bba4e140667f92d130b06b7b3c929a4003c9
Mehdi Benadda <me@mbenadda.com>
2020-02-10 11:15:22 +0100

    ✨(referrals) create a view for the Referral form
    We built the form by ourselves, relying on the django Form instance,
    to add bootstrap classes and lay out our form easily.

--- src/backend/partaj/core/views.py
+++ src/backend/partaj/core/views.py
@@ -2,7 +2,20 @@
 Views for our Core app.
 """
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .forms import ReferralForm
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    if request.method == "POST":
+        form = ReferralForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Referral saved")
+
+    else:
+        form = ReferralForm()
+
+    return render(request, 'core/new_referral.html', {"form": form})
