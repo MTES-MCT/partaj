48efd2a1de981e49be6b6ce40d82685f1fbfae6c
Mehdi Benadda <me@mbenadda.com>
2020-02-11 11:18:09 +0100

    ✨(backend) add static files URLs to partaj urlpatterns
    In order to serve static files, django needs the static files URLs
    to be added to the app's url patterns.

--- src/backend/partaj/urls.py
+++ src/backend/partaj/urls.py
@@ -1,4 +1,11 @@
+"""
+Partaj URLS.
+"""
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-urlpatterns = [path("", include("partaj.core.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("", include("partaj.core.urls")),
+    path("admin/", admin.site.urls),
+] + staticfiles_urlpatterns()
