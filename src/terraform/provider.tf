9678696022c6f805b8ff0d03f908e175efab1da2
Mehdi Benadda <me@mbenadda.com>
2020-02-13 11:07:56 +0100

    🔥(infra) remove Terraform setup for deployment on OVH
    Deploying on OVH turned out to be far from painless as it's just
    an IaaS and requires us to work through seldom-documented Terraform
    and OpenStack features to end up with a non-ideal setup where we'll
    have to manually handle our infra.
    
    We're opting for a PaaS offering instead to save time on this. Keep
    this history in git to reflect our experiments with deployment
    strategies.

--- src/terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-# Configure the OpenStack provider hosted by OVH
-provider "openstack" {
-  auth_url = "https://auth.cloud.ovh.net/v3"
-  domain_name = "default" # Domain Name - Always "default" for OVH
-  alias = "ovh"
-}
