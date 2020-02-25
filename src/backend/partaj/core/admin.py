089a011d70ca69d3d156f12b973500c9a4671dba
Mehdi Benadda <me@mbenadda.com>
2020-02-25 16:50:59 +0100

    ✨(backend) add ReferralAttachment to the Django admin
    Make sure we can use the Django admin facilities to manage file
    attachments to referrals along with the referrals themselves.
    
    Also add a TabularInline view for referral attachments in the
    admin view for a referral.

--- src/backend/partaj/core/admin.py
+++ src/backend/partaj/core/admin.py
@@ -3,9 +3,18 @@ Admin of the `core` app of the Partaj project.
 """
 from django.contrib import admin
 
-from .models import Referral
+from .models import Referral, ReferralAttachment
+
+
+@admin.register(ReferralAttachment)
+class ReferralAttachmentAdmin(admin.ModelAdmin):
+    pass
+
+
+class ReferralAttachmentInline(admin.TabularInline):
+    model = ReferralAttachment
 
 
 @admin.register(Referral)
 class ReferralAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ReferralAttachmentInline]
