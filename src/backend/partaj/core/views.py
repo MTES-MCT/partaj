39727cc24b8095ab86c4bba5b5e921dd0b4354e9
Mehdi Benadda <me@mbenadda.com>
2020-02-26 15:29:22 +0100

    ✨(referral) update the referral form view to allow file uploads
    Since we linked attached files to referrals through another model
    with a OneToMany relationship, we need to handle them manually in
    the view rather than just delegate to the ModelForm.
    We're adding in a file input, using the browser's default UI for now,
    and creating ReferralAttachment instances in the view handler for
    each uploaded file.

--- src/backend/partaj/core/views.py
+++ src/backend/partaj/core/views.py
@@ -5,15 +5,29 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import ReferralForm
+from .models import ReferralAttachment
 
 
 def index(request):
     if request.method == "POST":
-        form = ReferralForm(request.POST)
+        form = ReferralForm(request.POST, request.FILES)
 
         if form.is_valid():
-            form.save()
+            referral = form.save()
+
+            files = request.FILES.getlist('files')
+            for file in files:
+                referral_attachment = ReferralAttachment(
+                    file=file,
+                    name="some name",
+                    referral=referral,
+                )
+                referral_attachment.save()
+
             return HttpResponse("Referral saved")
+
+        else:
+            return HttpResponse(form.errors.as_text())
 
     else:
         form = ReferralForm()
