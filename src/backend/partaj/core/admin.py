de6e420d84c61798d717d8203ffea6196d747d5e
Mehdi Benadda <me@mbenadda.com>
2020-03-21 20:24:00 +0100

    ✨(admin) improve layout and data availability in Referral admin view
    The referral admin view was missing automatic fields such as id and
    most importantly creation date. As we added them, we took this
    opportunity to re-structure the Referral admin page in a way that
    makes sense for our users.

--- src/backend/partaj/core/admin.py
+++ src/backend/partaj/core/admin.py
@@ -2,6 +2,7 @@
 Admin of the `core` app of the Partaj project.
 """
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Referral, ReferralAttachment
 
@@ -17,4 +18,22 @@ class ReferralAttachmentInline(admin.TabularInline):
 
 @admin.register(Referral)
 class ReferralAdmin(admin.ModelAdmin):
+    # Show referral attachments inline on each referral
     inlines = [ReferralAttachmentInline]
+    # Display fields automatically created and updated by Django (as readonly)
+    readonly_fields = ["id", "created_at", "updated_at"]
+
+    # Organize data on the admin page
+    fieldsets = (
+        (_("Identification"), {"fields": ["id"]}),
+        (
+            _("Timing information"),
+            {"fields": ["created_at", "updated_at", "urgency", "urgency_explanation"]},
+        ),
+        (
+            _("Requester information"),
+            {"fields": ["requester", "requester_email", "requester_phone_number"]},
+        ),
+        (_("Metadata"), {"fields": ["subject", "status"]}),
+        (_("Referral content"), {"fields": ["question", "context", "prior_work"]}),
+    )
