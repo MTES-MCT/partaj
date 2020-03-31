cbaedd9a7115b2d9ab6bb4bb6863132733fb4089
Mehdi Benadda <me@mbenadda.com>
2020-03-31 16:38:46 +0200

    🐛(admin) set default referral attachment name in admin
    In the django admin site, referral attachments are created directly
    without ever going through our referral creation view. They're hence
    missing the part that defaults the referral attachment "name" field
    to the file name (without extension) if it is not filled by the user.
    
    We can subclass `ModelForm` to simply replicate this behavior in the
    admin site.

--- src/backend/partaj/core/admin.py
+++ src/backend/partaj/core/admin.py
@@ -1,10 +1,32 @@
 """
 Admin of the `core` app of the Partaj project.
 """
+import os
+
+from django import forms
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 from .models import Referral, ReferralAttachment
+
+
+class ReferralAttachmentForm(forms.ModelForm):
+    """
+    Custom admin form for referral attachment.
+    """
+
+    def clean(self):
+        """
+        Using the same approach we do in the user-facing view, default the referral attachment name
+        to the name of the file itself.
+        """
+        self._validate_unique = True
+
+        if not self.cleaned_data["name"]:
+            file_main, _ = os.path.splitext(self.cleaned_data["file"].name)
+            self.cleaned_data["name"] = file_main
+
+        return self.cleaned_data
 
 
 @admin.register(ReferralAttachment)
@@ -12,8 +34,13 @@ class ReferralAttachmentAdmin(admin.ModelAdmin):
     """
     Admin setup for referral attachments.
     """
+
     # Display fields automatically created and updated by Django (as readonly)
     readonly_fields = ["id", "created_at"]
+
+    # Customize behavior fo the referral attachment form to use the file name as the
+    # default name for the attachment
+    form = ReferralAttachmentForm
 
     # Organize data on the admin page
     fieldsets = (
@@ -38,6 +65,7 @@ class ReferralAttachmentInline(admin.TabularInline):
     """
     Let referral attachments be displayed inline on the referral admin view.
     """
+
     model = ReferralAttachment
 
 
@@ -46,6 +74,7 @@ class ReferralAdmin(admin.ModelAdmin):
     """
     Admin setup for referrals.
     """
+
     # Show referral attachments inline on each referral
     inlines = [ReferralAttachmentInline]
 
