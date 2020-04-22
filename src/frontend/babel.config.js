98b7e03e8e295be9f61c91fa3041f7a0328f522c
Mehdi Benadda <me@mbenadda.com>
2020-04-22 19:27:51 +0200

    🔧(frontend) configure the build for the frontend
    We'll be using react and typescript to make our frontend code, and
    building our bundle through webpack.
    Kickstart it with config files for babel and webpack.

---
+++ src/frontend/babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    [
+      'react-intl',
+      {
+        messagesDir: './i18n',
+      },
+    ],
+    ['@babel/plugin-syntax-dynamic-import'],
+  ],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3,
+        forceAllTransforms: true,
+        targets: 'last 1 version, >0.2%, IE 11',
+        useBuiltIns: 'usage',
+      },
+    ],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+};
