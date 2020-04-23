8994f22f2b1667b799b304758d6799c310ac060d
Mehdi Benadda <me@mbenadda.com>
2020-04-23 15:46:30 +0200

    ✨(frontend) create the skeleton of our frontend application
    Rather than build an SPA right now, we're planning on incorporating
    React-rendered components where user interaction happens and using
    server-side hacks makes no sense. We'll progressively extend our
    components where and when it makes sense.
    
    To achieve this, we need a skeleton with an index and a root component
    that uses React portals to render component to locations on the page
    that are tagged with a specially named class.
    
    This skeleton also takes care of wrapping everything in an i18n
    provider and loading whatever polyfills we need to run our app.

---
+++ src/frontend/js/components/Root/index.tsx
@@ -0,0 +1,45 @@
+import get from 'lodash-es/get';
+import startCase from 'lodash-es/startCase';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { ReferralDetailAssignment } from 'components/ReferralDetailAssignment';
+
+// Create a component map that we'll use below to access our component classes
+const componentLibrary = {
+  ReferralDetailAssignment,
+};
+
+interface RootProps {
+  partajReactSpots: Element[];
+}
+
+export const Root = ({ partajReactSpots }: RootProps) => {
+  const portals = partajReactSpots.map((element: Element) => {
+    // Generate a component name. It should be a key of the componentLibrary object / ComponentLibrary interface
+    const componentName = startCase(
+      get(element.className.match(/partaj-react--([a-zA-Z-]*)/), '[1]') || '',
+    )
+      .split(' ')
+      .join('') as keyof typeof componentLibrary;
+    // Get the component dynamically from the library of available components
+    const Component = componentLibrary[componentName];
+    // Sanity check: only attempt to render existing components
+    if (Component) {
+      // Get the incoming props to pass our component from the `data-props` attribute
+      const dataProps = element.getAttribute('data-props');
+      const props = dataProps ? JSON.parse(dataProps) : {};
+
+      return ReactDOM.createPortal(<Component {...props} />, element);
+    } else {
+      // Emit a warning at runtime when we fail to find a matching component for an element that required one
+      console.warn(
+        'Failed to load React component: no such component in Library ' +
+          componentName,
+      );
+      return null;
+    }
+  });
+
+  return <React.Fragment>{portals}</React.Fragment>;
+};
---
+++ src/frontend/js/utils/errors/index.tsx
@@ -0,0 +1,7 @@
+/**
+ * Generic placeholder for error handling. Allows us to send errors to the appropriate handler
+ * before we set up error monitoring facilities.
+ */
+export const handle = (error: Error) => {
+  console.error(error);
+}
