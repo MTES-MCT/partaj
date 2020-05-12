99a816e836381377681e896396dc2d1fd5a718ba
Mehdi Benadda <me@mbenadda.com>
2020-05-13 00:11:18 +0200

    🐛(frontend) fix <Spinner /> accessibility
    We recently learned that role="status" elements do not get an
    accessible name from their textual content. Which means we need
    to explicitly give them an accessible name.
    
    We can do that here by using aria-labelledby to point to the
    offscreen element that contains the textual content.

--- src/frontend/js/components/Spinner/index.tsx
+++ src/frontend/js/components/Spinner/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useUID } from 'react-uid';
 
 interface SpinnerProps {
   size?: 'small' | 'large';
@@ -7,10 +8,13 @@ interface SpinnerProps {
 
 /** Component. Displays a rotating CSS loader. */
 export const Spinner = ({ children, size }: SpinnerProps) => {
+  const uniqueID = useUID();
   return (
-    <div className="spinner-container" role="status">
+    <div className="spinner-container" role="status" aria-labelledby={uniqueID}>
       <div className={`spinner spinner--${size || 'small'}`} />
-      <div className="offscreen">{children}</div>
+      <div className="offscreen" id={uniqueID}>
+        {children}
+      </div>
     </div>
   );
 };
