036500039c5ab803a2b8a24e5c8f5ac6992a4cd2
Mehdi Benadda <me@mbenadda.com>
2020-05-14 21:14:57 +0200

    ✨(frontend) add a utility method for unit member permissions
    In addition to the existing method that checks if a user is an
    organizer for a given unit, we need one that checks if they're
    a member of any kind of that unit.
    
    We took this opportunity to add tests for the existing unit
    utility methods, which revealed a small issue with the handling
    of null.

--- src/frontend/js/utils/unit.ts
+++ src/frontend/js/utils/unit.ts
@@ -1,4 +1,5 @@
 import { Unit, User } from 'types';
+import { Nullable } from 'types/utils';
 
 /**
  * Get a list of organizers for a unit.
@@ -10,9 +11,16 @@ export const getUnitOrganizers = (unit: Unit) =>
   );
 
 /**
+ * Determine if a given user is a member of a given unit.
+ */
+export const isUserUnitMember = (user: Nullable<User>, unit: Unit) =>
+  !!user && unit.members.map((member) => member.id).includes(user.id);
+
+/**
  * Determine if a given user is an organizer for a given unit.
  */
-export const isUserUnitOrganizer = (user: User, unit: Unit) =>
+export const isUserUnitOrganizer = (user: Nullable<User>, unit: Unit) =>
+  !!user &&
   getUnitOrganizers(unit)
     .map((member) => member.id)
     .includes(user.id);
