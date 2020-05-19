7647f8fc7c67486c71e851c0517b3f56a53fc4b6
Mehdi Benadda <me@mbenadda.com>
2020-05-19 21:09:02 +0200

    ➕(styles) add tailwindcss to the project
    As we start building more custom elements in Partaj, starting with
    out timeline for the referral detail view, we'll switch from
    Bootstrap to tailwind. This should help us move from a quick
    prototyping phase to a new one where we built many more custom
    elements.

--- src/frontend/postcss.config.js
+++ src/frontend/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
+    require('tailwindcss'),
     require('autoprefixer'),
     require('cssnano')({
       preset: 'default',
