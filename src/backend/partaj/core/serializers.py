2afdf95a8d3fbbb517191dffc81e84f81c5fc42b
Mehdi Benadda <me@mbenadda.com>
2020-05-20 23:13:24 +0200

    ✨(api) add referral activities on referral objects in the API
    We want referral activities to be available on the referral as
    they are the way we primarily structure referral detail pages.
    We need to build them in the frontend as opposed to Django
    templates as we want to show them instantly when unit members
    create new events, and avoid relying on javascript page refereshing.

--- src/backend/partaj/core/serializers.py
+++ src/backend/partaj/core/serializers.py
@@ -5,7 +5,35 @@ and the JSON on the API.
 from rest_framework import serializers
 
 from partaj.users.models import User
-from .models import Referral, ReferralAnswer, Topic, Unit, UnitMembership
+from .models import (
+    Referral,
+    ReferralActivity,
+    ReferralAnswer,
+    Topic,
+    Unit,
+    UnitMembership,
+)
+
+
+class ReferralActivityItemField(serializers.RelatedField):
+    """
+    A custom field to use for the ReferralActivity item_content_object generic relationship.
+    """
+
+    def to_representation(self, value):
+        """
+        Serialize item content objects to their nested self.
+        """
+        if isinstance(value, User):
+            serializer = UserSerializer(value)
+        elif isinstance(value, ReferralAnswer):
+            serializer = ReferralAnswerSerializer(value)
+        else:
+            raise Exception(
+                "Unexpected type of related item content object on reeferral activity"
+            )
+
+        return serializer.data
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,6 +41,7 @@ class UserSerializer(serializers.ModelSerializer):
     Regular user serializer. Allowlist fields as user objects contain a lot more information
     than we'd like to expost on the API.
     """
+
     class Meta:
         model = User
         fields = [
@@ -33,6 +62,7 @@ class UnitMembershipSerializer(serializers.ModelSerializer):
     We want to specifically expose memberships on users when in the context of a unit so the
     frontend app can get information on their role in the unit.
     """
+
     class Meta:
         model = UnitMembership
         fields = "__all__"
@@ -45,6 +75,7 @@ class UnitMemberSerializer(serializers.ModelSerializer):
 
     This serializer should never be used for users outside of the context of a unit.
     """
+
     membership = serializers.SerializerMethodField()
 
     class Meta:
@@ -64,13 +95,16 @@ class UnitMemberSerializer(serializers.ModelSerializer):
         by a database constraint defined on the model, so we can safely use "get" here and be sure
         we do not miss anything.
         """
-        return UnitMembershipSerializer(member.unitmembership_set.get(unit=self.unit)).data
+        return UnitMembershipSerializer(
+            member.unitmembership_set.get(unit=self.unit)
+        ).data
 
 
 class UnitSerializer(serializers.ModelSerializer):
     """
     Unit serializer. Override default members to make use of the UnitMemberSerializer.
     """
+
     members = serializers.SerializerMethodField()
 
     class Meta:
@@ -81,7 +115,10 @@ class UnitSerializer(serializers.ModelSerializer):
         """
         Pass the unit id as an additional keyword argument to the unit member serializer.
         """
-        return [UnitMemberSerializer(member, unit=unit.id).data for member in unit.members.all()]
+        return [
+            UnitMemberSerializer(member, unit=unit.id).data
+            for member in unit.members.all()
+        ]
 
 
 class TopicSerializer(serializers.ModelSerializer):
@@ -89,6 +126,7 @@ class TopicSerializer(serializers.ModelSerializer):
     Topic serializer. Passthrough that allows us to more precisely specify downstream serializers,
     including unit, members and memberships.
     """
+
     unit = UnitSerializer()
 
     class Meta:
@@ -107,11 +145,22 @@ class ReferralAnswerSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class ReferralActivitySerializer(serializers.ModelSerializer):
+    actor = UserSerializer()
+    item_content_object = ReferralActivityItemField(read_only=True)
+
+    class Meta:
+        model = ReferralActivity
+        fields = "__all__"
+
+
 class ReferralSerializer(serializers.ModelSerializer):
     """
     Referral serializer. Uses our other serializers to limit available data on our nested objects
     and add relevant information where applicable.
     """
+
+    activity = ReferralActivitySerializer(many=True)
     answers = ReferralAnswerSerializer(many=True)
     topic = TopicSerializer()
     user = UserSerializer()
