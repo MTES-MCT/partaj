3cb7eb22876d7301ea0e508e793ee9df140089e1
Mehdi Benadda <me@mbenadda.com>
2020-05-25 14:24:13 +0200

    ✨(frontend) add a component to display a referral's content
    As we rebuild the structure of the referral detail view, we need it
    to handle more interaction, and we want to more tightly integrate
    the interactive parts with the plain content itself.
    
    To that end, we're moving it to the frontend, including the referral
    content.

---
+++ src/frontend/js/components/ReferralDetailContent/index.tsx
@@ -0,0 +1,160 @@
+import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import { Referral } from 'types';
+import { getUserFullname } from 'utils/user';
+
+const messages = defineMessages({
+  attachments: {
+    defaultMessage: 'Attachments',
+    description: "Subtitle for the referral's attachments list.",
+    id: 'components.ReferralDetailContent.attachments',
+  },
+  context: {
+    defaultMessage: 'Context',
+    description: "Subtitle for the referral's context.",
+    id: 'components.ReferralDetailContent.context',
+  },
+  expectedResponseTime: {
+    defaultMessage: 'Expected response time',
+    description: "Subtitle for the referral's expected response time.",
+    id: 'components.ReferralDetailContent.expectedResponseTime',
+  },
+  priorWork: {
+    defaultMessage: 'Prior work',
+    description: 'Subtitle for the prior work on the referral.',
+    id: 'components.ReferralDetailContent.priorWork',
+  },
+  officialRequester: {
+    defaultMessage: 'Official requester: {requesterName}',
+    description:
+      'Formal requester to whom the answer to the referral should be addressed.',
+    id: 'components.ReferralDetailContent.officialRequester',
+  },
+  question: {
+    defaultMessage: 'Referral question',
+    description: "Subtitle for the referral's question.",
+    id: 'components.ReferralDetailContent.question',
+  },
+  requester: {
+    defaultMessage: 'Requester',
+    description: 'Referral requester.',
+    id: 'components.ReferralDetailContent.requester',
+  },
+  requesterAs: {
+    defaultMessage: 'As {requesterName}',
+    description:
+      'User who actually signed up to Partaj to make the referral (as opposed to formal requester).',
+    id: 'components.ReferralDetailContent.requesterAs',
+  },
+  title: {
+    defaultMessage: 'Referral #{caseNumber}',
+    description: 'Title for the referral detail content view.',
+    id: 'components.ReferralDetailContent.title',
+  },
+  topic: {
+    defaultMessage: 'Referral topic',
+    description: 'Subtitle for the topic of the referral.',
+    id: 'components.ReferralDetailContent.topic',
+  },
+  urgencyExplanation: {
+    defaultMessage: 'Urgency explanation',
+    description: "Subtitle for the referral's urgency explanation.",
+    id: 'components.ReferralDetailContent.urgencyExplanation',
+  },
+});
+
+interface ReferralDetailContentProps {
+  referral: Referral;
+}
+
+export const ReferralDetailContent = ({
+  referral,
+}: ReferralDetailContentProps) => {
+  return (
+    <article
+      className="max-w-sm w-full lg:max-w-full border-gray-600 p-10 mt-8 mb-8 rounded-xl border"
+      aria-labelledby="referral-label-{{ referral.id }}"
+    >
+      <h3 className="text-4xl" id="referral-label-{{ referral.id }}">
+        <FormattedMessage
+          {...messages.title}
+          values={{ caseNumber: referral.id }}
+        />
+      </h3>
+
+      <h4 className="text-lg mt-6 mb-2 text-gray-600">
+        <FormattedMessage {...messages.requester} />
+      </h4>
+      <div className="font-semibold">
+        <FormattedMessage
+          {...messages.officialRequester}
+          values={{ requesterName: referral.requester }}
+        />
+      </div>
+      <div className="text-gray-600">
+        <FormattedMessage
+          {...messages.requesterAs}
+          values={{ requesterName: getUserFullname(referral.user) }}
+        />
+      </div>
+      <div className="text-gray-600">{referral.user.email}</div>
+      {referral.user.phone_number ? (
+        <div className="text-gray-600">{referral.user.phone_number}</div>
+      ) : null}
+
+      <h4 className="text-lg mt-6 mb-2 text-gray-600">
+        <FormattedMessage {...messages.topic} />
+      </h4>
+      <p>{referral.topic.name}</p>
+
+      <h4 className="text-lg mt-6 mb-2 text-gray-600">
+        <FormattedMessage {...messages.question} />
+      </h4>
+      <p className="user-content">{referral.question}</p>
+
+      <h4 className="text-lg mt-6 mb-2 text-gray-600">
+        <FormattedMessage {...messages.context} />
+      </h4>
+      <p className="user-content">{referral.context}</p>
+
+      <h4 className="text-lg mt-6 mb-2 text-gray-600">
+        <FormattedMessage {...messages.priorWork} />
+      </h4>
+      <p className="user-content">{referral.prior_work}</p>
+
+      {referral.attachments.length > 0 ? (
+        <>
+          <h4 className="text-lg mt-6 mb-2 text-gray-600">
+            <FormattedMessage {...messages.attachments} />
+          </h4>
+          <div className="list-group">
+            {referral.attachments.map((attachment) => (
+              <a
+                className="list-group-item list-group-item-action"
+                href={attachment.file}
+              >
+                {attachment.name_with_extension}
+                {attachment.size ? ` — ${attachment.size_human}` : null}
+              </a>
+            ))}
+          </div>
+        </>
+      ) : null}
+
+      <h4 className="text-lg mt-6 mb-2 text-gray-600">
+        <FormattedMessage {...messages.expectedResponseTime} />
+      </h4>
+      <p>{referral.urgency_human}</p>
+
+      {referral.urgency_explanation ? (
+        <>
+          <h4 className="text-lg mt-6 mb-2 text-gray-600">
+            <FormattedMessage {...messages.urgencyExplanation} />
+          </h4>
+          <p className="user-content">{referral.urgency_explanation}</p>
+        </>
+      ) : null}
+    </article>
+  );
+};
