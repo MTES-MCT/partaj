95838368e9cb28a4ee68f4592664c5f0e7b48668
Mehdi Benadda <me@mbenadda.com>
2020-06-03 16:30:50 +0200

    🔧(tests) add jest-dom to jest startup
    We already had jest-dom in our dependencies. As we tried to use it,
    we noticed it was not actually loaded in our tests. We used the
    recommended way to load it globally for all jest tests through a
    setup script.

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   moduleDirectories: [path.resolve(__dirname, 'js'), 'node_modules'],
   moduleFileExtensions: ['css', 'js', 'ts', 'tsx'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
   testMatch: [`${__dirname}/js/**/*.spec.+(ts|tsx|js)`],
   testURL: 'https://localhost',
   transformIgnorePatterns: ['node_modules/(?!(lodash-es)/)'],
