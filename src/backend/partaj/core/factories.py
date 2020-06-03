f80eaa14569830609d6e4d99322ec0768cea4399
Mehdi Benadda <me@mbenadda.com>
2020-06-03 16:09:27 +0200

    ✅(backend) add factories for unit membership & referral assignments
    As we prepared to write more tests for the referral "assign" and
    "unassign" calls, we noticed the lack of factories for unit membership
    and referral assignment, which would facilitate the writing of these
    tests.

--- src/backend/partaj/core/factories.py
+++ src/backend/partaj/core/factories.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 
 import factory
 
-from .models import Referral, Topic, Unit
+from .models import Referral, ReferralAssignment, Topic, Unit, UnitMembership, UnitMembershipRole
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -25,6 +25,22 @@ class UnitFactory(factory.django.DjangoModelFactory):
     name = factory.Faker("company")
 
 
+class UnitMembershipFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = UnitMembership
+
+    role = factory.Faker("word", ext_word_list=UnitMembershipRole.values)
+    user = factory.SubFactory(UserFactory)
+    unit = factory.SubFactory(UnitFactory)
+
+
+class UnitMemberFactory(UserFactory):
+    class Meta:
+        model = get_user_model()
+
+    membership = factory.RelatedFactory(UnitMembershipFactory, "user")
+
+
 class TopicFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Topic
@@ -43,3 +59,30 @@ class ReferralFactory(factory.django.DjangoModelFactory):
     requester = factory.Faker("name")
     topic = factory.SubFactory(TopicFactory)
     user = factory.SubFactory(UserFactory)
+
+
+class ReferralAssignmentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ReferralAssignment
+
+    referral = factory.SubFactory(ReferralFactory)
+    created_by = factory.SubFactory(UserFactory)
+    unit = factory.SubFactory(UnitFactory)
+
+    @factory.lazy_attribute
+    def assignee(self):
+        """
+        Generate a membership to the unit with a brand new user and make this new user
+        the assignee.
+        """
+        membership = UnitMembershipFactory(unit=self.unit)
+        return membership.user
+
+    @factory.lazy_attribute
+    def created_by(self):
+        """
+        Generate a membership to the unit with a brand new user and have this news user
+        be the the assignment creator.
+        """
+        membership = UnitMembershipFactory(unit=self.unit, role=UnitMembershipRole.OWNER)
+        return membership.user
