89d31ae1d16f7684ece506e6525410bfc45e6e17
Mehdi Benadda <me@mbenadda.com>
2020-06-10 17:59:38 +0200

    ✅(frontend) fix date strings in factories
    We need the dates on our objects to be strings. We use a `derived`
    call to make sure to transform the random dates generated by faker
    into strings.
    
    We needed to add a call to the faker function as it only returns
    a function, not the final value.

--- src/frontend/js/utils/test/factories.ts
+++ src/frontend/js/utils/test/factories.ts
@@ -3,7 +3,7 @@ import { compose, createSpec, derived, faker } from '@helpscout/helix';
 import { ReferralState, UnitMembershipRole, ReferralActivityVerb } from 'types';
 
 export const UserFactory = createSpec({
-  date_joined: derived(() => faker.date.past().toString()),
+  date_joined: derived(() => faker.date.past()().toISOString()),
   email: faker.internet.email(),
   first_name: faker.name.firstName(),
   id: faker.random.uuid(),
@@ -21,10 +21,10 @@ const UnitMembershipRoleFactory = createSpec(
 );
 
 export const UnitMembershipFactory = createSpec({
-  created_at: faker.date.past(),
+  created_at: derived(() => faker.date.past()().toISOString()),
   id: faker.random.uuid(),
   role: UnitMembershipRoleFactory,
-  updated_at: faker.date.past(),
+  updated_at: derived(() => faker.date.past()().toISOString()),
   user: faker.random.uuid(),
   unit: faker.random.uuid(),
 });
@@ -36,14 +36,14 @@ const UnitMemberMixin = createSpec({
 export const UnitMemberFactory = compose(UserFactory, UnitMemberMixin);
 
 export const UnitFactory = createSpec({
-  created_at: derived(() => faker.date.past().toString()),
+  created_at: derived(() => faker.date.past()().toISOString()),
   id: faker.random.uuid(),
   members: UnitMemberFactory.generate(1, 5),
   name: faker.company.companyName(),
 });
 
 export const TopicFactory = createSpec({
-  created_at: derived(() => faker.date.past().toString()),
+  created_at: derived(() => faker.date.past()().toISOString()),
   id: faker.random.uuid(),
   name: faker.lorem.words(),
   unit: UnitFactory,
@@ -55,7 +55,7 @@ const ReferralActivityVerbFactory = createSpec(
 
 export const ReferralActivityFactory = createSpec({
   actor: UserFactory,
-  created_at: derived(() => faker.date.past().toString()),
+  created_at: derived(() => faker.date.past()().toISOString()),
   id: faker.random.uuid(),
   item_content_object: null,
   item_content_type: faker.random.number(),
@@ -66,7 +66,7 @@ export const ReferralActivityFactory = createSpec({
 
 export const ReferralAnswerFactory = createSpec({
   content: faker.lorem.paragraphs(),
-  created_at: derived(() => faker.date.past().toString()),
+  created_at: derived(() => faker.date.past()().toISOString()),
   created_by: faker.random.uuid(),
   id: faker.random.uuid(),
   referral: faker.random.number(),
@@ -74,7 +74,7 @@ export const ReferralAnswerFactory = createSpec({
 
 export const ReferralAttachmentFactory = createSpec({
   id: faker.random.uuid(),
-  created_at: derived(() => faker.date.past().toString()),
+  created_at: derived(() => faker.date.past()().toISOString()),
   file: faker.internet.url(),
   name: faker.system.filePath(),
   name_with_extension: faker.system.fileName(),
@@ -93,14 +93,14 @@ export const ReferralFactory = createSpec({
   assignees: derived(() => []),
   attachments: ReferralAttachmentFactory.generate(1, 5),
   context: faker.lorem.paragraphs(),
-  created_at: derived(() => faker.date.past().toString()),
+  created_at: derived(() => faker.date.past()().toISOString()),
   id: faker.random.number(),
   prior_work: faker.lorem.paragraphs(),
   question: faker.lorem.paragraphs(),
   requester: faker.fake('{{name.firstName}} {{name.lastName}}'),
   state: derived(() => ReferralState.RECEIVED),
   topic: TopicFactory,
-  updated_at: derived(() => faker.date.past().toString()),
+  updated_at: derived(() => faker.date.past()().toISOString()),
   urgency: ReferralUrgencyFactory,
   urgency_explanation: faker.lorem.words(),
   user: UserFactory,
