b29d76ac8180860e8e53c40120d4200a755881dc
Mehdi Benadda <me@mbenadda.com>
2020-07-09 17:04:47 +0200

    ✨(frontend) add a small helper to get lists from our API
    Getting lists from our API is a repetitive task that will use the
    same URL structure and headers.
    
    We're adding a helper to avoid repeating this boilerplate all over
    the code. Its purpose is to be passed as a fetcher to react-query.

---
+++ src/frontend/js/data/fetchList.tsx
@@ -0,0 +1,19 @@
+import { Context } from 'types/context';
+
+/**
+ * `react-query` fetcher for lists of items from the Partaj API.
+ */
+export const fetchList = (context: Context) => async (name: string) => {
+  const response = await fetch(`/api/${name}/`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...(context.token ? { Authorization: `Token ${context.token}` } : {}),
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to get list of ${name}.`);
+  }
+
+  return await response.json();
+};
