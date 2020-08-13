b5e91cd891dc54f6dbe00b384b5d63428b57bda6
Mehdi Benadda <me@mbenadda.com>
2020-08-13 16:49:34 +0200

    🐛(frontend) fix missing element on list queries
    We have not implemented any kind of pagination on our list requests
    yet (on the frontend side). This was causing an issue with some of
    them, eg. the topics list on a new referral, where it was only getting
    and displaying the first 10 topics.
    
    Set a limit to 999 by default so we can ignore this issue for now and
    handle pagination/scaling properly later on, on a case by case basis.

--- src/frontend/js/data/fetchList.tsx
+++ src/frontend/js/data/fetchList.tsx
@@ -4,7 +4,7 @@ import { Context } from 'types/context';
  * `react-query` fetcher for lists of items from the Partaj API.
  */
 export const fetchList = (context: Context) => async (name: string) => {
-  const response = await fetch(`/api/${name}/`, {
+  const response = await fetch(`/api/${name}/?limit=999`, {
     headers: {
       'Content-Type': 'application/json',
       ...(context.token ? { Authorization: `Token ${context.token}` } : {}),
