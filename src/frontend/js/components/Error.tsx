72c4d28802e1307f1732bf2ea9ce828564017bee
Mehdi Benadda <me@mbenadda.com>
2020-10-06 16:24:43 +0200

    ✨(frontend) add a generic error component
    The generic <Error /> component has nothing to do with error handling,
    it simply shows a message to the user when something breaks in an
    unexpected place.

---
+++ src/frontend/js/components/Error.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+const messages = defineMessages({
+  error: {
+    defaultMessage:
+      'There was an error. Please try reloading the page or contact us at partaj@beta.gouv.fr.',
+    description:
+      'Generic error message to display when unexpected and unexplained errors happen.',
+    id: 'components.Error.error',
+  },
+});
+
+export const Error = () => (
+  <div>
+    <FormattedMessage {...messages.error} />
+  </div>
+);
