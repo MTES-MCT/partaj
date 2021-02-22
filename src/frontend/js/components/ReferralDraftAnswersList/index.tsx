eeca71abc62f941a0191796ef712de48d1e7d996
Mehdi Benadda <me@mbenadda.com>
2021-02-22 22:08:08 +0100

    ✨(frontend) create an empty state for draft answers list
    <ReferralDraftAnswersList /> was missing an empty state, which was
    making it very unhelpful when there were no existing draft answers.
    
    We added a simple explanation sentence and a button to create the
    first answer in order to make it more helpful for new users.

--- src/frontend/js/components/ReferralDraftAnswersList/index.tsx
+++ src/frontend/js/components/ReferralDraftAnswersList/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { defineMessages, FormattedDate, FormattedMessage } from 'react-intl';
 import { QueryStatus } from 'react-query';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useRouteMatch } from 'react-router-dom';
 
-import { Spinner } from 'components/Spinner';
+import { CreateAnswerButton } from 'components/CreateAnswerButton';
 import { GenericErrorMessage } from 'components/GenericErrorMessage';
 import { ReferralAnswerActions } from 'components/ReferralAnswerActions';
 import { ReferralAnswerStatusBadge } from 'components/ReferralAnswerStatusBadge';
+import { Spinner } from 'components/Spinner';
 import { useReferral, useReferralAnswers } from 'data';
 import { ReferralAnswerState } from 'types';
 import { getUserFullname } from 'utils/user';
 
 const messages = defineMessages({
+  emptyList: {
+    defaultMessage: 'There are no draft answers for this referral yet.',
+    description:
+      'Message for the empty state of the referral draft answers list.',
+    id: 'components.ReferralDraftAnswersList.emptyList',
+  },
   loadingAnswers: {
     defaultMessage: 'Loading referral answers...',
     description:
@@ -51,6 +58,7 @@ export const ReferralDraftAnswersList: React.FC<ReferralDraftAnswersListProps> =
   referralId,
 }) => {
   const history = useHistory();
+  const { url } = useRouteMatch();
 
   const { data: answersData, status: answersStatus } = useReferralAnswers({
     referral: String(referralId),
@@ -95,67 +103,83 @@ export const ReferralDraftAnswersList: React.FC<ReferralDraftAnswersListProps> =
           </tr>
         </thead>
         <tbody className="answers-list-table">
-          {answersData!.results
-            // We're only showing draft answers here as the actual published answer is just a copy
-            // of its corresponding draft answer.
-            .filter((answer) => answer.state === ReferralAnswerState.DRAFT)
-            .map((answer, index) => (
-              <tr
-                key={answer.id}
-                className={`stretched-link-container cursor-pointer hover:bg-gray-300 relative ${
-                  index % 2 === 0 ? 'bg-white' : 'bg-gray-100'
-                }`}
-                // Each row should have a higher z-index than the following one so the dropdown
-                // can freely overflow onto the row below it.
-                style={{ zIndex: 9999 - index }}
-                onClick={(event) => {
-                  // Link stretching does not work in Safari. JS has to take over to make rows clickable.
-                  // Also make sure to not break the dropdown menu and answer sending modal as we do that.
-                  if (
-                    document
-                      .querySelector('.answers-list-table')!
-                      .contains(event.target as Node) &&
-                    (!document.querySelector(`.answer-menu-${answer.id}`) ||
-                      !document
-                        .querySelector(`.answer-menu-${answer.id}`)!
-                        .contains(event.target as Node))
-                  ) {
-                    history.push(getAnswerUrl(answer.id));
-                  }
-                }}
-              >
-                <td>
-                  <Link
-                    to={getAnswerUrl(answer.id)}
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <FormattedDate
-                      year="numeric"
-                      month="numeric"
-                      day="numeric"
-                      value={answer.created_at}
-                    />
-                  </Link>
-                </td>
-                <td>
-                  <div>{getUserFullname(answer.created_by)}</div>
-                  <div className="text-gray-500">
-                    {answer.created_by.unit_name}
-                  </div>
-                </td>
-                <td>
-                  <ReferralAnswerStatusBadge answer={answer} />
-                </td>
-                <td>
-                  <div className={`answer-menu-${answer.id}`}>
-                    <ReferralAnswerActions
-                      answer={answer}
-                      referral={referral!}
-                    />
-                  </div>
-                </td>
-              </tr>
-            ))}
+          {answersData!.count === 0 ? (
+            <tr>
+              <td colSpan={4} className="text-center space-y-2">
+                <div>
+                  <FormattedMessage {...messages.emptyList} />
+                </div>
+                <div>
+                  <CreateAnswerButton
+                    getAnswerUrl={(answerId) => `${url}/${answerId}/form`}
+                    referral={referral!}
+                  />
+                </div>
+              </td>
+            </tr>
+          ) : (
+            answersData!.results
+              // We're only showing draft answers here as the actual published answer is just a copy
+              // of its corresponding draft answer.
+              .filter((answer) => answer.state === ReferralAnswerState.DRAFT)
+              .map((answer, index) => (
+                <tr
+                  key={answer.id}
+                  className={`stretched-link-container cursor-pointer hover:bg-gray-300 relative ${
+                    index % 2 === 0 ? 'bg-white' : 'bg-gray-100'
+                  }`}
+                  // Each row should have a higher z-index than the following one so the dropdown
+                  // can freely overflow onto the row below it.
+                  style={{ zIndex: 9999 - index }}
+                  onClick={(event) => {
+                    // Link stretching does not work in Safari. JS has to take over to make rows clickable.
+                    // Also make sure to not break the dropdown menu and answer sending modal as we do that.
+                    if (
+                      document
+                        .querySelector('.answers-list-table')!
+                        .contains(event.target as Node) &&
+                      (!document.querySelector(`.answer-menu-${answer.id}`) ||
+                        !document
+                          .querySelector(`.answer-menu-${answer.id}`)!
+                          .contains(event.target as Node))
+                    ) {
+                      history.push(getAnswerUrl(answer.id));
+                    }
+                  }}
+                >
+                  <td>
+                    <Link
+                      to={getAnswerUrl(answer.id)}
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <FormattedDate
+                        year="numeric"
+                        month="numeric"
+                        day="numeric"
+                        value={answer.created_at}
+                      />
+                    </Link>
+                  </td>
+                  <td>
+                    <div>{getUserFullname(answer.created_by)}</div>
+                    <div className="text-gray-500">
+                      {answer.created_by.unit_name}
+                    </div>
+                  </td>
+                  <td>
+                    <ReferralAnswerStatusBadge answer={answer} />
+                  </td>
+                  <td>
+                    <div className={`answer-menu-${answer.id}`}>
+                      <ReferralAnswerActions
+                        answer={answer}
+                        referral={referral!}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))
+          )}
         </tbody>
       </table>
     </div>
