435c61f65c00fc433d1dc02c3f552a8b8cc230a0
Mehdi Benadda <me@mbenadda.com>
2021-03-19 17:28:03 +0100

    ✨(frontend) add a <Tickbox /> component
    As part of our rework of the referral assignment components, we're
    visually styling our buttons like tickboxes to make it easier to
    see which people/units are assigned and which are not.

---
+++ src/frontend/js/components/Tickbox.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { appData } from 'appData';
+
+interface TickboxProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  isTicked: boolean;
+}
+
+export const Tickbox: React.FC<TickboxProps> = ({
+  className = '',
+  isTicked,
+  ...restProps
+}) => (
+  <div
+    className={`flex items-center justify-center w-5 h-5 bg-gray-700 rounded-sm text-white ${className}`}
+    {...restProps}
+  >
+    {isTicked ? (
+      <svg role="img" className="w-3 h-3 fill-current">
+        <use xlinkHref={`${appData.assets.icons}#icon-tick`} />
+      </svg>
+    ) : null}
+  </div>
+);
