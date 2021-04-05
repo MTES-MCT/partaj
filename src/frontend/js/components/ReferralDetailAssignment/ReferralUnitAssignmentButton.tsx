9a2f8f0ea066c0de964ccf3ce3b8e7a7ffa5a531
Mehdi Benadda <me@mbenadda.com>
2021-04-05 22:42:21 +0200

    ✨(frontend) update referral assignment menu to manage units
    Partaj had a referral assignment dropdown menu that allowed unit
    organizers to manage member assignments. As we wanted to also
    manage unit assignments, we had to rework the button and the dropdown
    menu to add tabs so both units and members can be managed from the
    same dropdown menu.
    
    We also had to make the dropdown available to non-organizer users
    so they can still get information on unit and member assignments
    even if they cannot make changes.

--- src/frontend/js/components/ReferralDetailAssignment/ReferralUnitAssignmentButton.tsx
+++ src/frontend/js/components/ReferralDetailAssignment/ReferralUnitAssignmentButton.tsx
@@ -3,34 +3,31 @@ import React from 'react';
 import { DropdownButton } from 'components/DropdownMenu';
 import { Tickbox } from 'components/Tickbox';
 import { useReferralAction } from 'data';
-import { Referral, UserLite } from 'types';
-import { getUserFullname } from 'utils/user';
+import { Referral, Unit } from 'types';
 
-interface ReferralMemberAssignmentButtonProps {
+interface ReferralUnitAssignmentButtonProps {
   isAssigned: boolean;
-  member: UserLite;
   referral: Referral;
+  unit: Unit;
 }
 
-export const ReferralMemberAssignmentButton: React.FC<ReferralMemberAssignmentButtonProps> = ({
+export const ReferralUnitAssignmentButton: React.FC<ReferralUnitAssignmentButtonProps> = ({
   isAssigned,
-  member,
   referral,
+  unit,
 }) => {
   const mutation = useReferralAction();
-  const relevantUnit = referral.units.filter((unit) =>
-    unit.members.some((unitMember) => unitMember.id === member.id),
-  )[0];
 
   return (
     <DropdownButton
       className="hover:bg-gray-100 focus:bg-gray-100"
+      style={{ height: '3.5rem' }}
       isLoading={mutation.isLoading}
       aria-pressed={isAssigned}
       onClick={() =>
         mutation.mutate({
-          action: isAssigned ? 'unassign' : 'assign',
-          payload: { assignee: member.id },
+          action: isAssigned ? 'unassign_unit' : 'assign_unit',
+          payload: { unit: unit.id },
           referral,
         })
       }
@@ -38,11 +35,8 @@ export const ReferralMemberAssignmentButton: React.FC<ReferralMemberAssignmentBu
       <div className="w-full flex flex-row items-center">
         <div className="flex flex-col flex-grow">
           <div className="truncate" style={{ maxWidth: 'calc(100% - 1.5rem)' }}>
-            {getUserFullname(member)}
+            {unit.name}
           </div>
-          {relevantUnit ? (
-            <div className="text-gray-500">{relevantUnit.name}</div>
-          ) : null}
         </div>
         {mutation.isLoading ? null : (
           <Tickbox aria-hidden={true} isTicked={isAssigned} />
