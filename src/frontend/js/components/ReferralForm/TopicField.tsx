56bfa1597ef954240d0ab809314094950036fc9d
Nicolas.Jourdain <nicolas.jourdain@developpement-durable.gouv.fr>
2021-04-16 11:11:43 +0000

    fix Frontend CI / lint-prettier error

--- src/frontend/js/components/ReferralForm/TopicField.tsx
+++ src/frontend/js/components/ReferralForm/TopicField.tsx
@@ -36,8 +36,7 @@ const messages = defineMessages({
     description:
       'Error message when the user selects an urgency level that requires justification ' +
       'and forgot to justify it.',
-    id:
-      'components.ReferralForm.TopicFields.errorChoiceTopic',
+    id: 'components.ReferralForm.TopicFields.errorChoiceTopic',
   },
 });
 
@@ -92,7 +91,6 @@ export const TopicField: React.FC<TopicFieldProps> = ({
   const [suggestions, setSuggestions] = useState<types.Topic[]>([]);
   const [value, setValue] = useState<string>('');
   const [isNoTopicselected, setisNoTopicselected] = useState<boolean>(false);
-  
 
   const getTopics: Autosuggest.SuggestionsFetchRequested = async ({
     value,
@@ -142,7 +140,6 @@ export const TopicField: React.FC<TopicFieldProps> = ({
     });
   }, [state.value, state.context]);
 
-
   return (
     <div className="mb-8">
       <label
@@ -162,7 +159,7 @@ export const TopicField: React.FC<TopicFieldProps> = ({
         onSuggestionsFetchRequested={getTopics}
         onSuggestionsClearRequested={() => setSuggestions([])}
         onSuggestionSelected={(_, { suggestion }) =>
-            send({ type: 'CHANGE', data: suggestion.id })
+          send({ type: 'CHANGE', data: suggestion.id })
         }
         getSuggestionValue={(topic) => topic.name}
         renderSuggestion={(topic, { isHighlighted }) => (
@@ -174,18 +171,19 @@ export const TopicField: React.FC<TopicFieldProps> = ({
           'aria-describedby': seed('referral-topic-description'),
           placeholder: intl.formatMessage(messages.label),
           onChange: (_, { newValue }) => {
-            if (state.context.value.length > 0 ) send({ type: 'CHANGE', data: '' })
+            if (state.context.value.length > 0)
+              send({ type: 'CHANGE', data: '' });
             setValue(newValue);
           },
-          onBlur:(()=> {  
-            (state.context.value.length === 0)? 
-              setisNoTopicselected(true): 
-              setisNoTopicselected(false)
-          }),
+          onBlur: () => {
+            state.context.value.length === 0
+              ? setisNoTopicselected(true)
+              : setisNoTopicselected(false);
+          },
           value,
         }}
       />
-       {isNoTopicselected ? (
+      {isNoTopicselected ? (
         <div className="mt-4 text-danger-600">
           <FormattedMessage {...messages.errorChoiceTopic} />
         </div>
