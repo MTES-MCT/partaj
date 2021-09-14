576f178ee09715194c5386ebc600c66563e1a4f1
Nicolas.Jourdain <nicolas.jourdain@developpement-durable.gouv.fr>
2021-09-14 07:10:13 +0000

    refuse referral frontend

---
+++ src/frontend/js/components/ReferralDetail/CloseReferralModal.tsx
@@ -0,0 +1,213 @@
+import React, { useState, useEffect } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import ReactModal from 'react-modal';
+import { useUIDSeed } from 'react-uid';
+
+import { GenericErrorMessage } from 'components/GenericErrorMessage';
+import { Spinner } from 'components/Spinner';
+import { useReferralAction } from 'data';
+import { Referral } from 'types';
+
+const messages = defineMessages({
+  cancel: {
+    defaultMessage: 'Cancel',
+    description: 'Button to cancel closing a referral and close the modal.',
+    id: 'components.ReferralDetail.CloseReferralModal.cancel',
+  },
+  formErrorMandatory: {
+    defaultMessage: 'Close this referral require an explanation.',
+    description:
+      'Error message when the user closes the referral without providing an explanation.',
+    id: 'components.ReferralDetail.CloseReferralModal.formErrorMandatory',
+  },
+  formErrorServer: {
+    defaultMessage:
+      'There was an error while updating the referral. Please retry later or contact an administrator.',
+    description:
+      'Error message when the close referral in the modal fails for an unknown reason.',
+    id: 'components.ReferralDetail.CloseReferralModal.formErrorServer',
+  },
+  formLabelExplanation: {
+    defaultMessage: 'close explanation',
+    description:
+      'Label for the change explanation field in the modal to close the referral.',
+    id: 'components.ReferralDetail.CloseReferralModal.formLabelExplanation',
+  },
+  modalTitle: {
+    defaultMessage: 'Close the referral.',
+    description:
+      'Title for the modal that allows unit members to close the referral.',
+    id: 'components.ReferralDetail.CloseReferralModal.modalTitle',
+  },
+  modalMessage: {
+    defaultMessage:
+      'This action will close the referral without an answer. You can provide an explanation to other stakeholders using the explanation field.',
+    description: 'Message warning user why close referral.',
+    id: 'components.ReferralDetail.CloseReferralModal.modalMessage',
+  },
+  update: {
+    defaultMessage: 'Close referral',
+    description: 'Button toclose the referral in the relevant modal.',
+    id: 'components.ReferralDetail.CloseReferralModal.update',
+  },
+});
+
+// The `setAppElement` needs to happen in proper code but breaks our testing environment.
+// This workaround is not satisfactory but it allows us to both test <SendAnswerModal />
+// and avoid compromising accessibility in real-world use.
+const isTestEnv = typeof jest !== 'undefined';
+if (!isTestEnv) {
+  ReactModal.setAppElement('#app-root');
+}
+
+interface CloseReferralModalProps {
+  isCloseReferralModalOpen: boolean;
+  setIsCloseReferralModalOpen: (isOpen: boolean) => void;
+  referral: Referral;
+}
+
+export const CloseReferralModal: React.FC<CloseReferralModalProps> = ({
+  isCloseReferralModalOpen,
+  setIsCloseReferralModalOpen,
+  referral,
+}) => {
+  const seed = useUIDSeed();
+  const mutation = useReferralAction();
+
+  // Keep track of the first form submission to show validation errors
+  const [isFormCleaned, setIsFormCleaned] = useState(false);
+
+  const [CloseReferralExplanation, setCloseReferralExplanation] = useState('');
+
+  useEffect(() => {
+    if (mutation.isSuccess) {
+      setIsCloseReferralModalOpen(false);
+      setIsFormCleaned(false);
+      setCloseReferralExplanation('');
+    }
+  }, [mutation.status]);
+
+  return (
+    <ReactModal
+      ariaHideApp={!isTestEnv}
+      isOpen={isCloseReferralModalOpen}
+      onRequestClose={() => setIsCloseReferralModalOpen(false)}
+      style={{
+        content: {
+          maxWidth: '32rem',
+          padding: '0',
+          position: 'static',
+        },
+        overlay: {
+          alignItems: 'center',
+          backgroundColor: 'rgba(0,0,0,0.75)',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        },
+      }}
+    >
+      <form
+        aria-labelledby={seed('close-referral-form')}
+        onSubmit={(e) => {
+          e.preventDefault();
+          setIsFormCleaned(true);
+          if (CloseReferralExplanation.length > 0) {
+            mutation.mutate({
+              action: 'close_referral',
+              payload: {
+                close_explanation: CloseReferralExplanation,
+              },
+              referral,
+            });
+          }
+        }}
+      >
+        <div className="p-8 space-y-4">
+          <h2 className="text-xl" id={seed('close-referral-form')}>
+            <FormattedMessage {...messages.modalTitle} />
+          </h2>
+
+          <p className="text-x2" id={seed('close-referral-message')}>
+            <FormattedMessage
+              {...messages.modalMessage}
+              values={{
+                linebreak: <br />,
+              }}
+            />
+          </p>
+
+          <div className="space-y-2">
+            <label
+              htmlFor={seed('close-referral-explanation')}
+              className="mb-1 font-semibold"
+            >
+              <FormattedMessage {...messages.formLabelExplanation} />
+            </label>
+            <textarea
+              className="form-control"
+              cols={40}
+              rows={4}
+              id={seed('close-referral-explanation')}
+              name="urgency-explanation"
+              value={CloseReferralExplanation}
+              onChange={(e) => {
+                setCloseReferralExplanation(e.target.value);
+              }}
+            />
+            {isFormCleaned &&
+            (CloseReferralExplanation! === undefined ||
+              CloseReferralExplanation.length === 0) ? (
+              <div className="mt-4 text-danger-600">
+                <FormattedMessage {...messages.formErrorMandatory} />
+              </div>
+            ) : null}
+          </div>
+
+          {mutation.isError ? (
+            <div className="text-danger-600">
+              <FormattedMessage {...messages.formErrorServer} />
+            </div>
+          ) : null}
+        </div>
+        <div className="flex justify-end bg-gray-300 p-8 space-x-4">
+          <button
+            className="btn btn-outline"
+            onClick={() => {
+              setCloseReferralExplanation('');
+              setIsCloseReferralModalOpen(false);
+            }}
+          >
+            <FormattedMessage {...messages.cancel} />
+          </button>
+
+          <button
+            type="submit"
+            className={`relative btn btn-primary ${
+              mutation.isLoading ? 'cursor-wait' : ''
+            }`}
+            aria-busy={mutation.isLoading}
+            aria-disabled={mutation.isLoading}
+          >
+            {mutation.isLoading ? (
+              <span aria-hidden="true">
+                <span className="opacity-0">
+                  <FormattedMessage {...messages.update} />
+                </span>
+                <Spinner
+                  size="small"
+                  color="white"
+                  className="absolute inset-0"
+                >
+                  {/* No children with loading text as the spinner is aria-hidden (handled by aria-busy) */}
+                </Spinner>
+              </span>
+            ) : (
+              <FormattedMessage {...messages.update} />
+            )}
+          </button>
+        </div>
+      </form>
+    </ReactModal>
+  );
+};
