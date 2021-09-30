103ba4c91581823d2c43e4971d7bb72f414099a7
Mehdi Benadda <me@mbenadda.com>
2021-09-30 11:27:52 +0200

    ✨(stats) replace the existing stats view with Metabase
    As we setup Metabase to track our stats, we can replace our existing,
    very limited stats view with a metabase embedded dashboard that
    will enable the team to enrich our stats page with more detailed
    information and remove that burden from us.

--- src/backend/partaj/core/views/stats.py
+++ src/backend/partaj/core/views/stats.py
@@ -1,11 +1,7 @@
 """
 General app statistics related views.
 """
-from django.contrib.auth import get_user_model
-from django.db.models import Count
 from django.views.generic.base import TemplateView
-
-from ..models import Referral, Unit
 
 
 class StatsView(TemplateView):
@@ -14,27 +10,3 @@ class StatsView(TemplateView):
     """
 
     template_name = "core/stats.html"
-
-    def get_context_data(self, **kwargs):
-        """
-        Get the statistics data we'll display in the templates.
-        """
-
-        context = super().get_context_data(**kwargs)
-
-        # Introductory statistics
-        context["referrals_count"] = Referral.objects.all().count()
-
-        # pylint: disable=invalid-name
-        User = get_user_model()
-        context["users_count"] = User.objects.all().count()
-
-        # Count units which have at least one linked topic (eg. can receive referrals)
-        context["units_count"] = (
-            Unit.objects.all()
-            .annotate(topics_count=Count("topic"))
-            .filter(topics_count__gt=0)
-            .count()
-        )
-
-        return context
