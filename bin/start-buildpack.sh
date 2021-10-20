73368b3849e43751a36592ee54d8f7d775ca8bde
Mehdi Benadda <me@mbenadda.com>
2021-10-20 15:21:50 +0200

    Deploy to production

--- bin/start-buildpack.sh
+++ bin/start-buildpack.sh
@@ -4,12 +4,10 @@
 # Start script to launch the project on hosts using buildpacks, such as Scalingo.
 ###
 
-# NB:
-
 # Build binary translation files for use by Django
 python manage.py compilemessages
 
 # The default commanddd runs gunicorn WSGI server
 python manage.py migrate
 
-gunicorn -c docker/files/usr/local/etc/gunicorn/partaj.py partaj.wsgi:application
+gunicorn -c gunicorn.py partaj.wsgi:application
