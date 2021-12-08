0c0c05f9a7fce2d51a8c247bada62c33e5444842
Mehdi Benadda <me@mbenadda.com>
2021-12-08 15:07:35 +0100

    🐛(frontend) fix contact email used in some error messages
    Some error messages use a contact email address to recommend users
    contact the Partaj team directly. The email provided was broken;
    replace it with the actual contact email address.

--- src/frontend/js/components/GenericErrorMessage.tsx
+++ src/frontend/js/components/GenericErrorMessage.tsx
@@ -4,7 +4,7 @@ import { defineMessages, FormattedMessage } from 'react-intl';
 const messages = defineMessages({
   error: {
     defaultMessage:
-      'There was an error. Please try reloading the page or contact us at partaj@beta.gouv.fr.',
+      'There was an error. Please try reloading the page or contact us at contact@partaj.beta.gouv.fr.',
     description:
       'Generic error message to display when unexpected and unexplained errors happen.',
     id: 'components.GenericErrorMessage.error',
