500c724e4af0d2fdfa3ef7647aa091c888289d8d
Mehdi Benadda <me@mbenadda.com>
2021-12-09 16:36:57 +0100

    ♻️(api) rework referral lite to use get_queryset
    As we prepare to add more filters and more kinds of filters on
    referral lite list queries, we decided to refactor our referral lite
    viewset to move filtering to the get_queryset method.
    This will enable us to add more filters in a scalable way and
    better manage separation of concerns.
    
    We also removed the separate task-based endpoints and changed tasks
    to be regular query parameters. This will enable us to reuse other
    filtering facilities while filtering on a given task in the same
    request.

--- src/backend/partaj/core/api/referral_lite.py
+++ src/backend/partaj/core/api/referral_lite.py
@@ -5,13 +5,15 @@ from django.contrib.auth import get_user_model
 from django.db.models import DateTimeField, Exists, ExpressionWrapper, F, OuterRef, Q
 
 import arrow
-from rest_framework import mixins, viewsets
-from rest_framework.decorators import action
+from rest_framework import exceptions, mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .. import models
 from ..serializers import ReferralLiteSerializer
+
+# pylint: disable=invalid-name
+User = get_user_model()
 
 
 class ReferralLiteViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -31,205 +33,149 @@ class ReferralLiteViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     ).annotate(unit=F("topic__unit__id"))
     serializer_class = ReferralLiteSerializer
 
+    def get_queryset(self):
+        """
+        Apply all relevant filters in the query parameters and return a ready-to-use queryset.
+        """
+        queryset = self.queryset.prefetch_related("assignees", "users")
+
+        unit = self.request.query_params.get("unit", None)
+        if unit is not None:
+            try:
+                unit = models.Unit.objects.get(id=unit)
+            except models.Unit.DoesNotExist as exc:
+                raise exceptions.ValidationError(
+                    detail=[f"Unit {unit} does not exist."]
+                ) from exc
+            # Make sure the user is a member of the unit and can make this request
+            try:
+                models.UnitMembership.objects.get(unit=unit, user=self.request.user)
+            except models.UnitMembership.DoesNotExist as exc:
+                raise exceptions.PermissionDenied from exc
+            queryset = queryset.filter(units=unit)
+
+        user = self.request.query_params.get("user", None)
+        if user is not None:
+            try:
+                user = User.objects.get(id=user)
+            except User.DoesNotExist as exc:
+                raise exceptions.ValidationError(
+                    detail=[f"User {user} does not exist."]
+                ) from exc
+            # Requests to filter on a given user can only be performed by said user themselves
+            if user != self.request.user:
+                raise exceptions.PermissionDenied
+            queryset = queryset.filter(users=user)
+
+        task = self.request.query_params.get("task", None)
+        if task == "answer_soon":
+            # Get a list of referrals that need to be answered soon if:
+            # - the user is assigned to this referral;
+            # - the user is owner on the related unit.
+            queryset = (
+                queryset.annotate(
+                    is_owned_by_user=Exists(
+                        models.UnitMembership.objects.filter(
+                            unit=OuterRef("topic__unit"),
+                            user=self.request.user,
+                            role=models.UnitMembershipRole.OWNER,
+                        )
+                    )
+                )
+                .annotate(
+                    is_user_assigned=Exists(
+                        models.ReferralAssignment.objects.filter(
+                            assignee=self.request.user,
+                            referral__id=OuterRef("id"),
+                        )
+                    ),
+                )
+                .exclude(
+                    (Q(is_owned_by_user=False) & Q(is_user_assigned=False))
+                    | Q(
+                        state__in=[
+                            models.ReferralState.ANSWERED,
+                            models.ReferralState.CLOSED,
+                        ]
+                    )
+                    | Q(due_date__gt=arrow.utcnow().shift(days=15).datetime),
+                )
+            )
+        elif task == "assign":
+            # Get a list of referrals up for assignment by the current user.
+            queryset = queryset.annotate(
+                is_owned_by_user=Exists(
+                    models.UnitMembership.objects.filter(
+                        unit=OuterRef("topic__unit"),
+                        user=self.request.user,
+                        role=models.UnitMembershipRole.OWNER,
+                    )
+                )
+            ).filter(is_owned_by_user=True, state=models.ReferralState.RECEIVED)
+        elif task == "process":
+            # Get a list of referrals up for processing by the current user.
+            queryset = (
+                queryset.annotate(
+                    has_active_assignment=Exists(
+                        models.ReferralAssignment.objects.filter(
+                            assignee=self.request.user,
+                            referral__id=OuterRef("id"),
+                            referral__state=models.ReferralState.ASSIGNED,
+                        )
+                    ),
+                )
+                .annotate(
+                    has_validation_request=Exists(
+                        models.ReferralAnswerValidationRequest.objects.filter(
+                            answer__referral__id=OuterRef("id")
+                        )
+                    ),
+                )
+                .filter(has_active_assignment=True, has_validation_request=False)
+            )
+        elif task == "validate":
+            # Get a list of referrals up for validation by the current user.
+            queryset = queryset.annotate(
+                has_active_validation_request=Exists(
+                    models.ReferralAnswerValidationRequest.objects.filter(
+                        answer__referral__id=OuterRef("id"),
+                        response=None,
+                        validator=self.request.user,
+                    )
+                )
+            ).filter(
+                has_active_validation_request=True,
+                state=models.ReferralState.IN_VALIDATION,
+            )
+        else:
+            # Make sure permissions cannot be bypassed by setting a bogus task
+            task = None
+
+        # Tasks manage their own authorization restrictions, otherwise unit/user filter is required
+        if task is None and unit is None and user is None:
+            raise exceptions.ValidationError(
+                detail=[
+                    "Referral list requests require at least a task/unit/user parameter."
+                ]
+            )
+
+        return queryset
+
     def list(self, request, *args, **kwargs):
         """
         Handle requests for lists of referrals. We're managing access rights inside the method
         as permissions depend on the supplied parameters.
         """
-        queryset = self.get_queryset().prefetch_related("assignees", "users")
+        try:
+            queryset = self.get_queryset()
+        except exceptions.ValidationError as exc:
+            return Response(status=400, data={"errors": exc.detail})
+        except exceptions.PermissionDenied:
+            # - request is filtering on a unit, but current user is not a member of that unit
+            # - request is filtering on a user who is no the current user
+            return Response(status=403)
 
-        unit = self.request.query_params.get("unit", None)
-        if unit is not None:
-            # Get the unit, return an error if it does not exist
-            try:
-                unit = models.Unit.objects.get(id=unit)
-            except models.Unit.DoesNotExist:
-                return Response(
-                    status=400, data={"errors": [f"Unit {unit} does not exist."]}
-                )
-
-            # Make sure the user is a member of the unit and can make this request
-            try:
-                models.UnitMembership.objects.get(unit=unit, user=request.user)
-            except models.UnitMembership.DoesNotExist:
-                return Response(status=403)
-
-            queryset = queryset.filter(units=unit).order_by("due_date")
-
-            page = self.paginate_queryset(queryset)
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
-
-            serializer = self.get_serializer(queryset, many=True)
-            return Response(serializer.data)
-
-        user = self.request.query_params.get("user", None)
-        if user is not None:
-            # Get the user, return an error if it does not exist
-            try:
-                # pylint: disable=invalid-name
-                User = get_user_model()
-                user = User.objects.get(id=user)
-            except User.DoesNotExist:
-                return Response(
-                    status=400, data={"errors": [f"User {user} does not exist."]}
-                )
-
-            if user != request.user:
-                return Response(status=403)
-
-            queryset = queryset.filter(users=user).order_by("due_date")
-
-            page = self.paginate_queryset(queryset)
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
-
-            serializer = self.get_serializer(queryset, many=True)
-            return Response(serializer.data)
-
-        return Response(
-            status=400,
-            data={"errors": ["Referral list requests require parameters"]},
-        )
-
-    @action(detail=False)
-    def to_answer_soon(self, request):
-        """
-        Get a list of referrals that need to be answered soon if:
-            - the user is assigned to this referral;
-            - the user is owner on the related unit.
-        """
-
-        queryset = (
-            self.get_queryset()
-            .annotate(
-                is_owned_by_user=Exists(
-                    models.UnitMembership.objects.filter(
-                        unit=OuterRef("topic__unit"),
-                        user=request.user,
-                        role=models.UnitMembershipRole.OWNER,
-                    )
-                )
-            )
-            .annotate(
-                is_user_assigned=Exists(
-                    models.ReferralAssignment.objects.filter(
-                        assignee=request.user,
-                        referral__id=OuterRef("id"),
-                    )
-                ),
-            )
-            .exclude(
-                (Q(is_owned_by_user=False) & Q(is_user_assigned=False))
-                | Q(
-                    state__in=[
-                        models.ReferralState.ANSWERED,
-                        models.ReferralState.CLOSED,
-                    ]
-                )
-                | Q(due_date__gt=arrow.utcnow().shift(days=15).datetime),
-            )
-            .order_by("due_date")
-        )
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False)
-    def to_assign(self, request):
-        """
-        Get a list of referrals up for assignment by the current user.
-        """
-
-        queryset = (
-            self.get_queryset()
-            .annotate(
-                is_owned_by_user=Exists(
-                    models.UnitMembership.objects.filter(
-                        unit=OuterRef("topic__unit"),
-                        user=request.user,
-                        role=models.UnitMembershipRole.OWNER,
-                    )
-                )
-            )
-            .filter(is_owned_by_user=True, state=models.ReferralState.RECEIVED)
-            .order_by("due_date")
-        )
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False)
-    def to_process(self, request):
-        """
-        Get a list of referrals up for processing by the current user.
-        """
-
-        queryset = (
-            self.get_queryset()
-            .annotate(
-                has_active_assignment=Exists(
-                    models.ReferralAssignment.objects.filter(
-                        assignee=request.user,
-                        referral__id=OuterRef("id"),
-                        referral__state=models.ReferralState.ASSIGNED,
-                    )
-                ),
-            )
-            .annotate(
-                has_validation_request=Exists(
-                    models.ReferralAnswerValidationRequest.objects.filter(
-                        answer__referral__id=OuterRef("id")
-                    )
-                ),
-            )
-            .filter(has_active_assignment=True, has_validation_request=False)
-            .order_by("due_date")
-        )
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False)
-    def to_validate(self, request):
-        """
-        Get a list of referrals up for validation by the current user.
-        """
-
-        queryset = (
-            self.get_queryset()
-            .annotate(
-                has_active_validation_request=Exists(
-                    models.ReferralAnswerValidationRequest.objects.filter(
-                        answer__referral__id=OuterRef("id"),
-                        response=None,
-                        validator=request.user,
-                    )
-                )
-            )
-            .filter(
-                has_active_validation_request=True,
-                state=models.ReferralState.IN_VALIDATION,
-            )
-            .order_by("due_date")
-        )
+        queryset = queryset.order_by("due_date")
 
         page = self.paginate_queryset(queryset)
         if page is not None:
