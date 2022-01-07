da2abc41fb7b14adee9d4cbc7e3e6e184fd9350e
Mehdi Benadda <me@mbenadda.com>
2022-01-07 15:14:20 +0100

    ✨(frontend) add an autocomplete field for units
    As we decided to use an autocompletion field to let users find units
    to filter on in referral table, we just had to clone the users
    autocomplete field, with some light changes, to make something similar
    for units.

---
+++ src/frontend/js/components/AutocompleteUnitField/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import Autosuggest, { InputProps } from 'react-autosuggest';
+import { QueryFunction, QueryKey, useQueryClient } from 'react-query';
+
+import { fetchList } from 'data/fetchList';
+import * as types from 'types';
+import { Nullable } from 'types/utils';
+
+interface AutocompleteUnitFieldProps {
+  filterSuggestions?: Nullable<(suggestions: types.Unit[]) => types.Unit[]>;
+  inputProps?: Partial<InputProps<types.Unit>>;
+  onSuggestionSelected: (suggestion: types.Unit) => void;
+}
+
+export const AutocompleteUnitField = ({
+  filterSuggestions,
+  inputProps = {},
+  onSuggestionSelected,
+}: AutocompleteUnitFieldProps) => {
+  const queryClient = useQueryClient();
+
+  const [value, setValue] = useState('');
+  const [suggestions, setSuggestions] = useState<types.Unit[]>([]);
+
+  const getUsers: Autosuggest.SuggestionsFetchRequested = async ({ value }) => {
+    const units: types.APIList<types.Unit> = await queryClient.fetchQuery(
+      ['units', { query: value }],
+      fetchList as QueryFunction<any, QueryKey>,
+    );
+    setSuggestions(
+      filterSuggestions ? filterSuggestions(units.results) : units.results,
+    );
+  };
+
+  return (
+    <Autosuggest
+      suggestions={suggestions}
+      onSuggestionsFetchRequested={getUsers}
+      onSuggestionsClearRequested={() => setSuggestions([])}
+      onSuggestionSelected={(_, { suggestion }) =>
+        onSuggestionSelected(suggestion)
+      }
+      getSuggestionValue={(unit) => unit.name}
+      renderSuggestion={(unit) => unit.name}
+      inputProps={{
+        ...inputProps,
+        onBlur: (_, event) => {
+          // If a given suggestion was highlighted, pick it as the selected unit
+          if (event?.highlightedSuggestion) {
+            onSuggestionSelected(event!.highlightedSuggestion);
+          }
+        },
+        onChange: (_, { newValue }) => {
+          setValue(newValue);
+          // Whenever there is a change in the value, if the new value matches one of our
+          // suggestions (the way the user sees it), pick it as the selected unit.
+          const suggestion = suggestions.find((unit) => unit.name === newValue);
+          if (suggestion) {
+            onSuggestionSelected(suggestion);
+          }
+        },
+        value,
+      }}
+    />
+  );
+};
