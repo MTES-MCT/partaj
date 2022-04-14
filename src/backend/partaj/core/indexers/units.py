93e469eff8e7e6b4e22a172f4bb1b3b6525e861f
Mehdi Benadda <me@mbenadda.com>
2022-04-15 00:51:25 +0200

    ✨(api) add ES-based list endpoints for units to the API
    To improve filtering on lists of referrals (and power up other
    features), we decided to create new endpoints to get lists of units
    quickly based on various kinds of parameters. To that end,
    we're setting up new viewsets that return "lite" units that are
    lighter and don't require authorization to view.
    
    As we add autocomplete based filters to the general purpose search
    field in <ReferralTable />, we also need to enable autocompletion
    for the filter values we're adding in the search field. We can reuse
    the Autocomplete mixin we set up for topics.

---
+++ src/backend/partaj/core/indexers/units.py
@@ -0,0 +1,69 @@
+"""
+Methods and configuration related to the indexing of Unit objects.
+"""
+from django.conf import settings
+
+from ..models import Unit
+from .common import partaj_bulk, slice_string_for_completion
+
+
+class UnitsIndexer:
+    """
+    Makes available the parameters the indexer requires as well as functions to shape
+    objects getting into and out of ElasticSearch.
+    """
+
+    index_name = f"{settings.ELASTICSEARCH['INDICES_PREFIX']}units"
+    mapping = {
+        "properties": {
+            "autocomplete": {
+                "type": "completion",
+                "analyzer": "simple_diacritics_insensitive",
+            },
+            "name": {"type": "keyword"},
+        }
+    }
+
+    @classmethod
+    def get_es_document_for_unit(cls, unit, index=None, action="index"):
+        """Build an Elasticsearch document from the unit instance."""
+        index = index or cls.index_name
+
+        unit_lite = {
+            "id": unit.id,
+            "name": unit.name,
+        }
+
+        return {
+            "_id": unit.id,
+            "_index": index,
+            "_op_type": action,
+            "_lite": unit_lite,
+            "autocomplete": slice_string_for_completion(
+                unit.name, delimiters=[" ", "/"]
+            ),
+            "name": unit.name,
+        }
+
+    @classmethod
+    def get_es_documents(cls, index=None, action="index"):
+        """
+        Loop on all the units in database and format them for the ElasticSearch index.
+        """
+        index = index or cls.index_name
+
+        for unit in Unit.objects.all():
+            yield cls.get_es_document_for_unit(unit, index=index, action=action)
+
+    @classmethod
+    def update_unit_document(cls, unit):
+        """
+        Update one document in Elasticsearch, corresponding to one Unit instance.
+        """
+
+        action = cls.get_es_document_for_unit(
+            unit=unit, index=cls.index_name, action="index"
+        )
+
+        # Use bulk to be able to reuse "get_es_document_for_unit" as-is.
+        partaj_bulk([action])
