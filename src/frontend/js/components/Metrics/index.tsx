9242bcf280300e8a0998ee97ca2bb5945295896c
Nicolas.Jourdain <nicolas.jourdain@developpement-durable.gouv.fr>
2022-05-23 12:52:55 +0000

    add metabase dasbords to sidebar

--- src/frontend/js/components/Metrics/index.tsx
+++ src/frontend/js/components/Metrics/index.tsx
@@ -1,6 +1,7 @@
 import { defineMessages } from '@formatjs/intl';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useParams } from 'react-router-dom';
 
 const messages = defineMessages({
   title: {
@@ -10,14 +11,26 @@ const messages = defineMessages({
   },
 });
 
+interface MetricsRouteParams {
+  metrics: string;
+}
+
 export const Metrics = () => {
+  const { metrics } = useParams<MetricsRouteParams>();
+  let src = '';
+
+  metrics === 'metricsDAJ'
+    ? (src =
+        'https://metabase.partaj.incubateur.net/public/dashboard/4eea24ec-acb1-4dec-a283-e74377d8faae')
+    : (src =
+        'https://metabase.partaj.incubateur.net/public/dashboard/0a9b14f3-1a1c-421c-8510-9a9d372b7a83');
   return (
     <section className="container mx-auto flex-grow flex flex-col">
       <h1 className=" float-left text-4xl my-4">
         <FormattedMessage {...messages.title} />
       </h1>
       <iframe
-        src="https://metabase.partaj.incubateur.net/public/dashboard/4eea24ec-acb1-4dec-a283-e74377d8faae"
+        src={src}
         frameBorder="0"
         width="1000"
         height="2300"
