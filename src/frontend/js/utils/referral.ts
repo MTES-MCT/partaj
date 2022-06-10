5ac32ef17cd7f8996e73573b474dfeccde36dbd7
Adrien Jézégou <adrien@qeety.com>
2022-06-10 23:31:55 +0200

    Add translations after ReferralDetails refacto

--- src/frontend/js/utils/referral.ts
+++ src/frontend/js/utils/referral.ts
@@ -1,8 +1,5 @@
-import { defineMessages } from 'react-intl';
-
-import { Referral, Unit, UnitMembershipRole, User } from 'types';
+import { Referral, User } from 'types';
 import { Nullable } from 'types/utils';
-import * as types from '../types';
 
 /**
  * Check if a given user is a referral requester
@@ -11,16 +8,13 @@ export const userIsRequester = (user: Nullable<User>, referral: Referral) =>
   user &&
   referral.users
     .map((user) => user.id)
-    .includes(user?.id || '$' /* impossible id */);
+    .includes(user.id);
 
 /**
  * Check if a given user is from referral affected unit
  */
-export const userIsUnitMember = (user: Nullable<User>, referral: Referral) => {
-  const tieps =
-    user &&
-    user.memberships.some((membership: { unit: string }) =>
-      referral!.units.map((unit) => unit.id).includes(membership.unit),
-    );
-  return tieps;
-};
+export const userIsUnitMember = (user: Nullable<User>, referral: Referral) =>
+  user &&
+  user.memberships.some((membership: { unit: string }) =>
+    referral!.units.map((unit) => unit.id).includes(membership.unit),
+  );
