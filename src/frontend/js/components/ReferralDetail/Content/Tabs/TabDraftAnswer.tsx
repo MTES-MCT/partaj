70f4bb6e96bc4316426cb972bedc728c976f06aa
Adrien Jézégou <adrien@qeety.com>
2022-06-20 17:25:25 +0200

    Add min css for referral answer tab v2

--- src/frontend/js/components/ReferralDetail/Content/Tabs/TabDraftAnswer.tsx
+++ src/frontend/js/components/ReferralDetail/Content/Tabs/TabDraftAnswer.tsx
@@ -3,6 +3,7 @@ import { Referral } from 'types';
 import { DownloadReferralButton } from '../../Buttons/DowloadReferralBtn';
 import { ReferralConversation } from '../../../ReferralConversation';
 import { ReferralDraftAnswerV2 } from '../../../ReferralDraftAnswerV2';
+import { useUIDSeed } from 'react-uid';
 
 interface TabDraftAnswersProps {
   referral: Referral;
@@ -11,11 +12,18 @@ interface TabDraftAnswersProps {
 export const TabDraftAnswer: React.FC<TabDraftAnswersProps> = ({
   referral,
 }) => {
+  const seed = useUIDSeed();
+
   return (
-    <>
-      <DownloadReferralButton referralId={String(referral!.id)} />
+    <article
+      className="w-full lg:max-w-4xl border-gray-400 p-10 mt-8 mb-8 rounded-xl border"
+      aria-labelledby={seed('referral-answer')}
+    >
+      <div className="flex space-x-4 float-right">
+        <DownloadReferralButton referralId={String(referral!.id)} />
+      </div>
       <ReferralDraftAnswerV2 />
       <ReferralConversation />
-    </>
+    </article>
   );
 };
