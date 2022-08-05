d0f98247ca29e773e8168381c7aa70a1776e1f3c
Adrien Jézégou <adrien@qeety.com>
2022-08-05 10:31:54 +0200

    Add command to send referral answer to notix

---
+++ src/backend/partaj/core/management/commands/send_response_to_notix.py
@@ -0,0 +1,62 @@
+"""
+Send referral's answer to notix
+"""
+import logging
+
+from django.core.management.base import BaseCommand
+
+from ... import models, services
+from ...models import Referral, ReferralAnswer
+from ...requests.note_api_request import NoteApiRequest
+
+logger = logging.getLogger("partaj")
+
+
+class Command(BaseCommand):
+    """
+    send a list of referral's answer to Notix
+
+    """
+
+    help = __doc__
+
+    def add_arguments(self, parser):
+        """
+        Define arguments
+        """
+        parser.add_argument("from", type=int)
+        parser.add_argument("to", type=int)
+
+    # pylint: disable=broad-except
+    def handle(self, *args, **options):
+        """
+        Send  referral's answer to notix
+        """
+        # Keep track of starting time for logging purposes
+        logger.info("Starting to sending response to Notix...")
+
+        api_note_request = NoteApiRequest()
+
+        for referral in Referral.objects.all()[options["from"] : options["to"]]:
+            try:
+                if services.FeatureFlagService.get_referral_version(referral) == 0:
+                    referral_answer = ReferralAnswer.objects.filter(
+                        state=models.ReferralAnswerState.PUBLISHED,
+                        referral__id=referral.id,
+                    ).last()
+
+                    if referral_answer:
+                        api_note_request.post_note(referral_answer)
+                        logger.info(
+                            "Referral n° %s : notice created with success.", referral.id
+                        )
+
+            except ValueError as exception:
+
+                for i in exception.args:
+                    logger.info(i)
+                logger.info("Referral n° %s :failed to create notice.", referral.id)
+
+            except Exception as exception:
+                for i in exception.args:
+                    logger.info(i)
