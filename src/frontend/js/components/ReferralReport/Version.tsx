a9d07dee7c191d6b7d2631576408d1d09f999281
Adrien Jézégou <adrien@qeety.com>
2022-09-22 10:58:18 +0200

    WIPWIP report design IconText are not working?

--- src/frontend/js/components/ReferralReport/Version.tsx
+++ src/frontend/js/components/ReferralReport/Version.tsx
@@ -31,71 +31,89 @@ export const Version: React.FC<VersionProps> = ({
   const [isModalOpen, setModalOpen] = useState(false);
   const [activeVersion, setActiveVersion] = useState(0);
 
+  const getFileName = (fileFullName: string) => {
+    const splittedFileName = fileFullName.split('.');
+    splittedFileName.pop();
+
+    return splittedFileName.join('.');
+  };
+
+  const getFileExtension = (fileFullName: string) => {
+    return `.${fileFullName.split('.').pop()}`;
+  };
+
   return (
     <>
-      <tr
+      <div
         data-testid="version"
         key={version.id}
-        className={`stretched-link-container relative`}
+        className={`flex flex-col relative bg-white p-3 rounded`}
       >
-        <td> Version {index + 1}</td>
-        <td>
+        <div className={`flex justify-between font-medium`}>
+          <span>Version {index + 1}</span>
+
+          <span>
+            {version.created_by.first_name} {version.created_by.last_name}
+          </span>
+        </div>
+
+        <div className={`flex justify-between text-sm text-gray-500`}>
           <FormattedDate
             year="numeric"
             month="long"
             day="numeric"
             value={version.updated_at}
           />
-        </td>
-        <td>
-          <p>
-            {version.created_by.first_name} {version.created_by.last_name}
-          </p>
-          <p>{version.created_by.unit_name}</p>
-        </td>
-        <td>
+          <div>
+            <p>{version.created_by.unit_name}</p>
+          </div>
+        </div>
+
+        <div className={`flex items-start`}>
           <a
             className="text-primary-500 hover:underline focus:underline"
             href={version.document.file}
             key={version.document.id}
           >
-            {version.document.name_with_extension}
-          </a>
-        </td>
-        <td>
-          {versionsLength === index + 1 && (
-            <div className="flex space-x-4">
-              {!referralIsPublished(referral) && (
-                <>
-                  {isAuthor(currentUser, version) && (
-                    <div data-testid="update-version-button">
-                      <ButtonFileUploader
-                        onSuccess={(result) => {
-                          onUpdateSuccess(result, index);
-                        }}
-                        onError={(error) => onUpdateError(error)}
-                        action={'PUT'}
-                        url={urls.versions + version.id + '/'}
-                      >
-                        Modifier
-                      </ButtonFileUploader>
-                    </div>
-                  )}
-                  <button
-                    data-testid="send-report"
-                    onClick={() => {
-                      setModalOpen(true);
-                      setActiveVersion(index + 1);
-                    }}
-                  >
-                    Envoyer
-                  </button>
-                </>
-              )}
+            <div className="bg-purple-300">
+              {getFileExtension(version.document.name_with_extension)}
             </div>
-          )}
-        </td>
-      </tr>
+            <div>
+              {getFileName(version.document.name_with_extension)}
+            </div>
+          </a>
+        </div>
+        {versionsLength === index + 1 && !referralIsPublished(referral) && (
+          <div className="flex w-full relative h-8 items-center">
+            {isAuthor(currentUser, version) && (
+              <div className="absolute left-0" data-testid="update-version-button">
+                <ButtonFileUploader
+                  onSuccess={(result) => {
+                    onUpdateSuccess(result, index);
+                  }}
+                  onError={(error) => onUpdateError(error)}
+                  action={'PUT'}
+                  url={urls.versions + version.id + '/'}
+                >
+                  Modifier
+                </ButtonFileUploader>
+              </div>
+            )}
+
+            <div className="absolute right-0">
+              <button
+                data-testid="send-report-button"
+                onClick={() => {
+                  setModalOpen(true);
+                  setActiveVersion(index + 1);
+                }}
+              >
+                Envoyer
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
       <SendVersionModal
         report={report}
         isModalOpen={isModalOpen}
