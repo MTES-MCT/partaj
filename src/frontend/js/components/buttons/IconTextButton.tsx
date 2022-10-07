2b7b542ac33528117f6f35d9ee1ea65e531ab9bc
Adrien Jézégou <adrien@qeety.com>
2022-10-07 14:30:17 +0200

    add other class for button

--- src/frontend/js/components/buttons/IconTextButton.tsx
+++ src/frontend/js/components/buttons/IconTextButton.tsx
@@ -4,6 +4,7 @@ interface IconTextButtonProps {
   children: ReactNode;
   icon: ReactNode;
   cssClass?: string;
+  otherClasses?: string;
   testId?: string;
   onClick: MouseEventHandler;
 }
@@ -12,12 +13,13 @@ export const IconTextButton = ({
   children,
   cssClass = 'default',
   testId = '',
+  otherClasses = '',
   onClick,
   icon,
 }: IconTextButtonProps) => {
   return (
     <button
-      className={`btn btn-${cssClass} rounded-sm pt-1 pb-1 pr-2 pl-2 flex items-center`}
+      className={`btn btn-${cssClass} ${otherClasses} rounded-sm pt-1 pb-1 pr-2 pl-2 flex items-center`}
       onClick={onClick}
       data-testid={testId}
     >
