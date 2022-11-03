f2b79ccedbd147a59709cc0a195e4597c01ba529
Adrien Jézégou <adrien@qeety.com>
2022-11-03 18:20:05 +0100

    Add_referral_v2_doc

--- src/frontend/js/components/ReferralDetail/Content/Tabs/TabReport.tsx
+++ src/frontend/js/components/ReferralDetail/Content/Tabs/TabReport.tsx
@@ -12,12 +12,17 @@ export const TabReport: React.FC<TabReportProps> = ({ referral }) => {
   return (
     <>
       <div className="w-full text-black border border-blue-300 bg-blue-200 p-4">
-        Une nouvelle version de projet de réponse est désormais disponible ! Pour toute information sur le fonctionnement n'hésitez pas à consulter
-          <a
+        Une nouvelle version de projet de réponse est désormais disponible !
+        Pour toute information sur le fonctionnement n'hésitez pas à consulter
+        <a
           className="navbar-nav-item space-x-2"
           target="_blank"
-          href="https://documentation.partaj.beta.gouv.fr"
-      > la documentation </a> ou nous contacter par email à contact@partaj.fr
+          href="https://documentation.partaj.beta.gouv.fr/guide-de-traitement-dune-saisine-1#traiter-une-saisine-qui-ma-ete-affectee-apres-le-04-11-2022"
+        >
+          {' '}
+          la documentation{' '}
+        </a>{' '}
+        ou à nous contacter par tchat
       </div>
       <div className="flex space-x-4 float-right mb-8">
         <DownloadReferralButton referralId={String(referral!.id)} />
