c761109a6c0e9dded4bf1db7c4dfebc2a70eca4d
Adrien Jézégou <adrien@qeety.com>
2022-11-03 17:53:56 +0100

    WIPWIP

--- src/frontend/js/components/ReferralDetail/Content/Tabs/TabReport.tsx
+++ src/frontend/js/components/ReferralDetail/Content/Tabs/TabReport.tsx
@@ -11,6 +11,14 @@ interface TabReportProps {
 export const TabReport: React.FC<TabReportProps> = ({ referral }) => {
   return (
     <>
+      <div className="w-full text-black border border-blue-300 bg-blue-200 p-4">
+        Une nouvelle version de projet de réponse est désormais disponible ! Pour toute information sur le fonctionnement n'hésitez pas à consulter
+          <a
+          className="navbar-nav-item space-x-2"
+          target="_blank"
+          href="https://documentation.partaj.beta.gouv.fr"
+      > la documentation </a> ou nous contacter par email à contact@partaj.fr
+      </div>
       <div className="flex space-x-4 float-right mb-8">
         <DownloadReferralButton referralId={String(referral!.id)} />
       </div>
