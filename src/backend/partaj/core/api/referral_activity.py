44d5f25445a1b1bf093bd75a6f4fe01e45015f51
Adrien Jézégou <adrien@qeety.com>
2022-11-15 13:40:34 +0100

    Add permissions/access for activity and messages for userfromrequestersunit

--- src/backend/partaj/core/api/referral_activity.py
+++ src/backend/partaj/core/api/referral_activity.py
@@ -51,7 +51,7 @@ class ReferralActivityViewSet(viewsets.ReadOnlyModelViewSet):
         queryset = self.queryset.filter(referral=referral)
 
         if (
-            request.user in referral.users.all()
+            referral.is_user_from_unit_referral_requesters(request.user)
             and not referral.units.filter(members__id=request.user.id).exists()
         ):
             linked_user_visible_activities = [
