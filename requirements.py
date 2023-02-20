e0b5121b94a8ad84a00f5cfc0aa06b5e5be5c799
Adrien Jézégou <adrien@qeety.com>
2023-02-20 18:26:34 +0100

    test fix python version

--- requirements.py
+++ requirements.py
@@ -7,6 +7,12 @@ Instead, we can use configparser to generate a requirements.txt file from our se
 """
 import configparser
 
+
+# Write the raw list of runtime to a runtime.txt file in the deployment directory
+runtime_file = open('../deployment/runtime.txt', 'w')
+runtime_file.write("python-3.7.5")
+runtime_file.close()
+
 # Read our setup.cfg file
 config = configparser.ConfigParser()
 config.read("src/backend/setup.cfg")
