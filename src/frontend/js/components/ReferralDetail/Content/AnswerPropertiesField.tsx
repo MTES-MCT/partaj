56f15d49e74466bfbaa763fc3a86dbf7c6fdbd02
Adrien Jézégou <adrien@qeety.com>
2023-03-07 14:16:17 +0100

    Add staff feature to specify v1 answertype

---
+++ src/frontend/js/components/ReferralDetail/Content/AnswerPropertiesField.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useReferralAction } from 'data';
+import { Referral } from 'types';
+
+export const AnswerPropertiesField = ({ referral }: { referral: Referral }) => {
+  const mutation = useReferralAction({
+    onSuccess: (data) => {},
+  });
+
+  return (
+    <div>
+      <select
+        className="form-control pb-1  pt-1"
+        id="answer-options"
+        name="answer-options"
+        value={referral.answer_properties}
+        onChange={(e) => {
+          mutation.mutate({
+            action: 'update_answer_properties',
+            payload: {
+              value: e.target.value,
+            },
+            referral,
+          });
+        }}
+      >
+        <option key={'value-none'} value={''}>
+          {'N/A'}
+        </option>
+        {referral.answer_options.map((answerOption) => (
+          <option key={answerOption.value} value={answerOption.value}>
+            {answerOption.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
