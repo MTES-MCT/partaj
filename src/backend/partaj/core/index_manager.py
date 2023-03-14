4e768a7b65aa57fd4c59d318a4b2227973a9c39e
Adrien Jézégou <adrien@qeety.com>
2023-03-14 12:44:36 +0100

    Remove html notelite

--- src/backend/partaj/core/index_manager.py
+++ src/backend/partaj/core/index_manager.py
@@ -51,6 +51,29 @@ def perform_create_index(indexable, logger=None):
     return new_index
 
 
+def perform_create_empty_index(indexable, logger=None):
+    """
+    Create a new index in ElasticSearch from an indexable instance.
+    """
+    # Create a new index name, suffixing its name with a timestamp
+    new_index = f"{indexable.index_name:s}_{timezone.now():%Y-%m-%d-%Hh%Mm%S.%fs}"
+
+    # Create the new index
+    if logger:
+        logger.info(f'Creating a new Elasticsearch index "{new_index:s}"...')
+    ES_INDICES_CLIENT.create(index=new_index)
+
+    # The index needs to be closed before we set an analyzer
+    ES_INDICES_CLIENT.close(index=new_index)
+    ES_INDICES_CLIENT.put_settings(body=ANALYSIS_SETTINGS, index=new_index)
+    ES_INDICES_CLIENT.open(index=new_index)
+
+    ES_INDICES_CLIENT.put_mapping(body=indexable.mapping, index=new_index)
+
+    # Return the name of the index we just created in ElasticSearch
+    return new_index
+
+
 def regenerate_indices(logger=None):
     """
     Create new indices for our indexables and replace possible existing indices with
@@ -134,22 +157,22 @@ def regenerate_indices(logger=None):
         ES_INDICES_CLIENT.delete(index=useless_index, ignore=[400, 404])
 
 
-def regenerate_index(logger=None):
+def regenerate_note_index(logger=None):
     """
-    Create new index for our indexable and replace possible existing index with
+    Create new index for our note index and replace possible existing index with
     a new one only once it has successfully built it.
     """
-    # Get all existing indices once; we'll look up into this list many times
+    # Get existing index
     try:
-        existing_indices = ES_INDICES_CLIENT.get_alias(NotesIndexer.index_name)
+        existing_index = ES_INDICES_CLIENT.get_alias(NotesIndexer.index_name)
     except NotFoundError:
         # Provide a fallback empty list so we don't have to check for its existence later on
-        existing_indices = {}
+        existing_index = {}
 
-    # Create a new index for each of those modules
+    # Create a new index for this module
     # NB: we're mapping perform_create_index which produces side-effects
     indices_to_create = zip(
-        list(map(lambda ix: perform_create_index(ix, logger), [NotesIndexer])),
+        list(map(lambda ix: perform_create_empty_index(ix, logger), [NotesIndexer])),
         [NotesIndexer],
     )
 
@@ -161,8 +184,7 @@ def regenerate_index(logger=None):
 
     # Get the previous indices for every alias
     indices_to_unalias = reduce(
-        lambda acc, ix: acc
-        + list(get_indices_by_alias(existing_indices, ix.index_name)),
+        lambda acc, ix: acc + list(get_indices_by_alias(existing_index, ix.index_name)),
         [NotesIndexer],
         [],
     )
@@ -174,17 +196,7 @@ def regenerate_index(logger=None):
         for index, alias in indices_to_unalias
     ]
 
-    # Identify orphaned indices that belong to our own app.
-    # NB: we *must* do this before the update_aliases call so we don't immediately prune
-    # version n-1 of all our indices
-    useless_indices = [
-        index
-        for index, details in existing_indices.items()
-        if index.startswith(str(settings.ELASTICSEARCH["INDICES_PREFIX"]))
-        and not details["aliases"]
-    ]
-
-    # Replace the old indices with the new ones in 1 atomic operation to avoid outage
+    # Replace the old index with the new one in 1 atomic operation to avoid outage
     def perform_aliases_update():
         try:
             ES_INDICES_CLIENT.update_aliases(
@@ -208,11 +220,3 @@ def regenerate_index(logger=None):
                 raise exception
 
     perform_aliases_update()
-
-    for useless_index in useless_indices:
-        # Disable keyword arguments checking as elasticsearch-py uses a decorator to list
-        # valid query parameters and inject them as kwargs. I won't say a word about this
-        # anti-pattern.
-        #
-        # pylint: disable=unexpected-keyword-arg
-        ES_INDICES_CLIENT.delete(index=useless_index, ignore=[400, 404])
