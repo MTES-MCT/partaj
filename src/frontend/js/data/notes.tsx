a7e09697821f0c9a41803c2ff579372e25fa3ecd
Adrien Jézégou <adrien@qeety.com>
2023-04-28 16:51:58 +0200

    Notes - Add publication date filter

--- src/frontend/js/data/notes.tsx
+++ src/frontend/js/data/notes.tsx
@@ -4,7 +4,11 @@ import { fetchList, FetchListQueryParams } from './fetchList';
 import { Note, NoteLite } from '../types';
 import { createOne } from './createOne';
 import { fetchOne } from './fetchOne';
-import { NoteFilters } from '../components/Notes/NoteListView';
+import {
+  FilterKeys,
+  NoteFilter,
+  NoteFilters,
+} from '../components/Notes/NoteListView';
 
 // Details
 type UseNoteDetailsActionOptions = UseMutationOptions<
@@ -54,6 +58,11 @@ type NoteListActionResponse = {
     };
   };
 };
+
+type NoteListActionParams = { query: string } & {
+  [key in FilterKeys]: Array<string>;
+};
+
 type UseNoteListActionParams = NoteFilters & {
   query: string;
 };
@@ -67,14 +76,33 @@ export const useNoteLitesAction = (options?: UseNoteListActionOptions) => {
   const queryClient = useQueryClient();
 
   return useMutation<NoteListActionResponse, unknown, UseNoteListActionParams>(
-    ({ query, topic, requesters_unit_names, assigned_units_names, author }) =>
-      notesLitesAction({
+    ({
+      query,
+      topic,
+      requesters_unit_names,
+      assigned_units_names,
+      author,
+      publication_date_before,
+      publication_date_after,
+    }) => {
+      return notesLitesAction({
         query,
-        topic,
-        requesters_unit_names,
-        assigned_units_names,
-        author,
-      }),
+        topic: topic.map((filter) => filter.value),
+        requesters_unit_names: requesters_unit_names.map(
+          (filter) => filter.value,
+        ),
+        assigned_units_names: assigned_units_names.map(
+          (filter) => filter.value,
+        ),
+        publication_date_after: publication_date_after.map(
+          (filter) => filter.value,
+        ),
+        publication_date_before: publication_date_before.map(
+          (filter) => filter.value,
+        ),
+        author: author.map((filter) => filter.value),
+      });
+    },
     {
       ...options,
       onSuccess: (data, variables, context) => {
@@ -103,7 +131,7 @@ export const noteDetailsAction = (params: UseNoteDetailsActionParams) => {
   return fetchOne({ queryKey: ['notes', params.id] });
 };
 
-export const notesLitesAction = (params: UseNoteListActionParams) => {
+export const notesLitesAction = (params: NoteListActionParams) => {
   return fetchList({
     queryKey: ['noteslites', params as FetchListQueryParams],
   });
