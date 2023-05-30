f26e4044c87dc87f662098614e1b5a794ccac729
Thomas Roger Lux <thomas.roger-lux@developpement-durable.gouv.fr>
2023-05-30 16:52:41 +0200

    Add requester unit informations in form, database and request infos
    Fix python tests
    
    Fix python tests

--- src/frontend/js/components/ReferralUsers/ReferralUsersBlock.tsx
+++ src/frontend/js/components/ReferralUsers/ReferralUsersBlock.tsx
@@ -6,7 +6,7 @@ import { IconTextButton } from '../buttons/IconTextButton';
 import { AddIcon } from '../Icons';
 import { ReferralContext } from '../../data/providers/ReferralProvider';
 import { ReferralUsersModalContext } from '../../data/providers/ReferralUsersModalProvider';
-import { ReferralState } from '../../types';
+import { ReferralState, RequesterUnitType } from '../../types';
 import { DescriptionText } from '../styled/text/DescriptionText';
 import { useUIDSeed } from 'react-uid';
 
@@ -32,6 +32,22 @@ const messages = defineMessages({
     description: 'CTA add users button title',
     id: 'components.ReferralUsersBlock.addUser',
   },
+  decentralisedContactBlockTitle: {
+    defaultMessage: 'Referent business service',
+    description: 'Title for the decentralised service contact block',
+    id: 'components.ReferralUsersBlock.decentralisedContactBlockTitle',
+  },
+  decentralisedContactBlockDescription: {
+    defaultMessage:
+      'The requester from a decentralised service contacted the following business service before requesting the DAJ. You can contact it in order to get additional informations.',
+    description: 'Description for the decentralised service contact block',
+    id: 'components.ReferralUsersBlock.decentralisedContactBlockDescription',
+  },
+  decentralisedContact: {
+    defaultMessage: 'Email contact: {email}',
+    description: 'Referent contact for the decentralised service',
+    id: 'components.ReferralUsersBlock.decentralisedContact',
+  },
 });
 
 export const ReferralUsersBlock: React.FC = () => {
@@ -39,11 +55,50 @@ export const ReferralUsersBlock: React.FC = () => {
   const { openRUModal } = useContext(ReferralUsersModalContext);
   const seed = useUIDSeed();
 
+  const showDecentralisedContactBlock =
+    referral?.state !== ReferralState.DRAFT &&
+    referral?.requester_unit_type === RequesterUnitType.DECENTRALISED_UNIT &&
+    referral?.requester_unit_contact;
+
   return (
     <>
       {referral && (
         <>
           <div className={`w-full space-y-4`}>
+            {showDecentralisedContactBlock && (
+              <div
+                className={
+                  'px-6 py-4 bg-gray-200 border-gray-400 rounded border'
+                }
+              >
+                <label
+                  htmlFor={seed('referral-users-label')}
+                  className="mb-1 font-semibold"
+                >
+                  <FormattedMessage
+                    {...messages.decentralisedContactBlockTitle}
+                  />
+                </label>
+                <DescriptionText>
+                  <FormattedMessage
+                    {...messages.decentralisedContactBlockDescription}
+                  />
+                </DescriptionText>
+                <FormattedMessage
+                  {...messages.decentralisedContact}
+                  values={{
+                    email: (
+                      <a
+                        className="text-primary-500 underline"
+                        href={`mailto:${referral.requester_unit_contact}`}
+                      >
+                        {referral.requester_unit_contact}
+                      </a>
+                    ),
+                  }}
+                />
+              </div>
+            )}
             {referral.state === ReferralState.DRAFT ? (
               <div>
                 <label
