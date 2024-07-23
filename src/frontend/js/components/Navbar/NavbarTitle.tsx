26e63844594809a8968944054d67c21f73cd1253
AdJez <adrien.jezegou@beta.gouv.fr>
2024-07-23 12:22:37 +0100

    sidebar WIPWIP

--- src/frontend/js/components/Navbar/NavbarTitle.tsx
+++ src/frontend/js/components/Navbar/NavbarTitle.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 type SidebarTitleProps = React.PropsWithChildren<{}>;
 
 export const NavbarTitle: React.FC<SidebarTitleProps> = ({ children }) => {
-  return <div className="navbar-title">{children}</div>;
+  return <div className="text-lg">{children}</div>;
 };
