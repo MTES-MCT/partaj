d3b2fcd29a1b1e0f4473a3eb9fad7b153de07d72
AdJez <adrien.jezegou@beta.gouv.fr>
2024-12-11 13:50:46 +0100

    Fix redirect

--- src/frontend/js/components/Dashboard.tsx
+++ src/frontend/js/components/Dashboard.tsx
@@ -3,32 +3,22 @@ import { useFeatureFlag } from '../data';
 import { DashboardProvider } from './NewDashboard/DashboardContext';
 import { NewDashboard } from './NewDashboard';
 import { OldDashboard } from './OldDashboard';
-import { useCurrentUser } from '../data/useCurrentUser';
-import { hasMembership } from '../utils/user';
-import { Redirect } from 'react-router-dom';
 
 export const Dashboard: React.FC = () => {
   const { status, data } = useFeatureFlag('new_dashboard');
-  const { currentUser } = useCurrentUser();
 
   return (
     <>
-      {currentUser && hasMembership(currentUser) ? (
+      {status === 'success' && (
         <>
-          {status === 'success' && (
-            <>
-              {data?.is_active ? (
-                <DashboardProvider>
-                  <NewDashboard />
-                </DashboardProvider>
-              ) : (
-                <OldDashboard />
-              )}
-            </>
+          {data?.is_active ? (
+            <DashboardProvider>
+              <NewDashboard />
+            </DashboardProvider>
+          ) : (
+            <OldDashboard />
           )}
         </>
-      ) : (
-        <Redirect to="/my-dashboard" />
       )}
     </>
   );
