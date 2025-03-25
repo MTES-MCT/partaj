71690830b4028c5802ef96862592d5255777df23
Thomas Roger Lux <thomas.roger-lux@developpement-durable.gouv.fr>
2025-03-25 11:08:40 +0100

    Add middleware for security headers

---
+++ src/backend/partaj/middleware.py
@@ -0,0 +1,12 @@
+class HeadersMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        response["X-Content-Type-Options"] = "nosniff"
+        response["X-XSS-Protection"] = 0
+        response["Content-Security-Policy"] = "upgrade-insecure-requests"
+
+        return response
