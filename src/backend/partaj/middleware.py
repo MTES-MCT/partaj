95c490f8326cd120ea98eca515ab1a543382b7a8
Thomas Roger Lux <thomas.roger-lux@developpement-durable.gouv.fr>
2025-04-17 16:00:41 +0200

    Update Content Security Policy header

--- src/backend/partaj/middleware.py
+++ src/backend/partaj/middleware.py
@@ -16,6 +16,8 @@ class HeadersMiddleware:
 
         response["X-Content-Type-Options"] = "nosniff"
         response["X-XSS-Protection"] = 0
-        response["Content-Security-Policy"] = "upgrade-insecure-requests"
+        response["Content-Security-Policy"] = (
+            "default-src 'self'; connect-src 'self' *.partaj.incubateur.net ws: *.crisp.chat; style-src 'self' 'unsafe-inline' *.crisp.chat *.partaj.incubateur.net; img-src 'self' *.din.developpement-durable.gouv.fr *.crisp.chat data:; script-src 'self' 'unsafe-inline' *.crisp.chat *.partaj.incubateur.net; font-src 'self' *.crisp.chat; frame-src *.partaj.incubateur.net"
+        )
 
         return response
