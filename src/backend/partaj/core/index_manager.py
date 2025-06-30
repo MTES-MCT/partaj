c34c246cdc8e4c46489f0869fac35d9f8ead4145
AdJez <adrien.jezegou@i-carre.net>
2025-06-30 15:22:17 +0200

    Add batch referral indexation

--- src/backend/partaj/core/index_manager.py
+++ src/backend/partaj/core/index_manager.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 
 from elasticsearch.exceptions import NotFoundError, RequestError
 
-from partaj.core.indexers import NotesIndexer
+from partaj.core.indexers import NotesIndexer, ReferralsIndexer
 
 from .indexers import ES_INDICES, ES_INDICES_CLIENT
 from .indexers.common import partaj_bulk
@@ -87,10 +87,12 @@ def regenerate_indices(logger=None):
         existing_indices = []
 
     # Create a new index for each of those modules
-    # NB: we're mapping perform_create_index which produces side-effects
+    # NB: we're mapping perform_create_index which produces side effects
     indices_to_create = zip(
         list(map(lambda ix: perform_create_index(ix, logger), ES_INDICES)), ES_INDICES
     )
+
+    # ->
 
     # Prepare to alias them so they can be swapped-in for the previous versions
     actions_to_create_aliases = [
@@ -186,6 +188,73 @@ def regenerate_note_index(logger=None):
     indices_to_unalias = reduce(
         lambda acc, ix: acc + list(get_indices_by_alias(existing_index, ix.index_name)),
         [NotesIndexer],
+        [],
+    )
+
+    # Prepare to un-alias them so they can be swapped-out for the new versions
+    # NB: use chain to flatten the list of generators
+    actions_to_delete_aliases = [
+        {"remove": {"index": index, "alias": alias}}
+        for index, alias in indices_to_unalias
+    ]
+
+    # Replace the old index with the new one in 1 atomic operation to avoid outage
+    def perform_aliases_update():
+        try:
+            ES_INDICES_CLIENT.update_aliases(
+                dict(actions=actions_to_create_aliases + actions_to_delete_aliases)
+            )
+        except RequestError as exception:
+            # This operation can fail if an index exists with the same name as an alias we're
+            # attempting to create. In our case, this is not supposed to happen and is usually
+            # the result of a broken ES state.
+            if exception.error == "invalid_alias_name_exception":
+                # We have to extract the name of the broken index from a string
+                pattern = re.compile(r"Invalid alias name \[(.*)\].*")
+                broken_index = pattern.match(exception.info["error"]["reason"]).group(1)
+                # Delete it (it was unusable and we can recreate its data at-will)
+                ES_INDICES_CLIENT.delete(index=broken_index)
+                # Attempt to perform the operation again
+                # We're doing this recursively in case more than one such broken indices existed
+                perform_aliases_update()
+            # Let other kinds of errors be raised
+            else:
+                raise exception
+
+    perform_aliases_update()
+
+
+def regenerate_referral_index(logger=None):
+    """
+    Create new index for our referral index and replace possible existing index with
+    a new one only once it has successfully built it.
+    """
+    # Get existing index
+    try:
+        existing_index = ES_INDICES_CLIENT.get_alias(ReferralsIndexer.index_name)
+    except NotFoundError:
+        # Provide a fallback empty list so we don't have to check for its existence later on
+        existing_index = {}
+
+    # Create a new index for this module
+    # NB: we're mapping perform_create_index which produces side-effects
+    indices_to_create = zip(
+        list(
+            map(lambda ix: perform_create_empty_index(ix, logger), [ReferralsIndexer])
+        ),
+        [ReferralsIndexer],
+    )
+
+    # Prepare to alias them so they can be swapped-in for the previous versions
+    actions_to_create_aliases = [
+        {"add": {"index": index, "alias": ix.index_name}}
+        for index, ix in indices_to_create
+    ]
+
+    # Get the previous indices for alias
+    indices_to_unalias = reduce(
+        lambda acc, ix: acc + list(get_indices_by_alias(existing_index, ix.index_name)),
+        [ReferralsIndexer],
         [],
     )
 
