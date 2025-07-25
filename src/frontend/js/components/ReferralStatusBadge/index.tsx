5db40749e0aca999afeead30a146ce09a1a2820c
AdJez <adrien.jezegou@i-carre.net>
2025-07-25 10:39:06 -0400

    Add referral status on subref tooltip

--- src/frontend/js/components/ReferralStatusBadge/index.tsx
+++ src/frontend/js/components/ReferralStatusBadge/index.tsx
@@ -32,21 +32,34 @@ const stateStyles = {
 
 interface ReferralStatusBadgeProps {
   status: ReferralState;
+  size?: 'base' | 'sm';
 }
 
 export const ReferralStatusBadge: React.FC<ReferralStatusBadgeProps> = ({
   status,
+  size = 'base',
 }) => {
   const badgeMessage = referralStateMessages.hasOwnProperty(status)
     ? referralStateMessages[status]
     : null;
 
+  const getTextSize = (size: 'base' | 'sm') => {
+    switch (size) {
+      case 'sm':
+        return 'text-xxs';
+      default:
+        return 'text-s';
+    }
+  };
+
   return (
     <div
-      className={`font-marianne flex items-center capitalize badge whitespace-nowrap ${classes[status]}`}
+      className={`font-marianne flex items-center capitalize ${
+        size === 'sm' ? 'badge-sm' : 'badge'
+      } whitespace-nowrap ${classes[status]}`}
     >
       <div className={`${stateStyles[status]}`}></div>
-      <span className="text-s text-black uppercase mb-0.5">
+      <span className={`${getTextSize(size)} text-black uppercase mb-0.5`}>
         {badgeMessage && <FormattedMessage {...badgeMessage} />}
       </span>
     </div>
