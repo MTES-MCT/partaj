cd6c41fdc79f42d461076a3f40d5ed082ec1c406
AdJez <adrien.jezegou@i-carre.net>
2025-07-25 09:54:47 -0400

    Disable adding user when referral is in a splitting state

--- src/frontend/js/components/ReferralDetail/Content/Tabs/TabUsers.tsx
+++ src/frontend/js/components/ReferralDetail/Content/Tabs/TabUsers.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ReferralUsersBlock } from '../../../ReferralUsers/ReferralUsersBlock';
 import { ReferralUsersModalProvider } from '../../../../data/providers/ReferralUsersModalProvider';
 import { RoleModalProvider } from '../../../../data/providers/RoleModalProvider';
 import { Modals } from '../../../modals/Modals';
+import { ReferralContext } from '../../../../data/providers/ReferralProvider';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { ReferralState } from 'types';
+
+const messages = defineMessages({
+  unavailableRequesterTab: {
+    defaultMessage: 'Adding requesters is not avaiblable in a splitting mode',
+    description: 'Unavailable requester tab text',
+    id: 'components.ReferralTabs.unavailableRequesterTab',
+  },
+});
 
 export const TabUsers: React.FC = () => {
+  const { referral } = useContext(ReferralContext);
+
   return (
     <>
-      <ReferralUsersModalProvider>
-        <RoleModalProvider>
-          <ReferralUsersBlock />
-          <Modals />
-        </RoleModalProvider>
-      </ReferralUsersModalProvider>
+      {referral &&
+        ([ReferralState.SPLITTING, ReferralState.RECEIVED_SPLITTING].includes(
+          referral.state,
+        ) ? (
+          <div>
+            <FormattedMessage {...messages.unavailableRequesterTab} />
+          </div>
+        ) : (
+          <ReferralUsersModalProvider>
+            <RoleModalProvider>
+              <ReferralUsersBlock />
+              <Modals />
+            </RoleModalProvider>
+          </ReferralUsersModalProvider>
+        ))}
     </>
   );
 };
