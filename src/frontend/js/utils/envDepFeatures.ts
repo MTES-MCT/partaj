37d7db10eca429daf50849ea6a7c4aad41d9e183
AdJez <adrien.jezegou@i-carre.net>
2025-07-31 09:56:56 -0400

    Fix menu stats display for MASA

--- src/frontend/js/utils/envDepFeatures.ts
+++
@@ -1,6 +0,0 @@
-import { appData } from '../appData';
-
-/**
- * Indicates if statistics feature has to be shown for the user
- */
-export const hasStatisticsFeature = () => appData.env_version === 'MTES';
