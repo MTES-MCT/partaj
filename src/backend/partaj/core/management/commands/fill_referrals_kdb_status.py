11ce12f9bb54bc4c415a32a5eb109cd89ec8dc8f
Thomas Roger Lux <thomas.roger-lux@developpement-durable.gouv.fr>
2025-09-30 15:12:32 +0200

    WIP to handle override after referrals are published

---
+++ src/backend/partaj/core/management/commands/fill_referrals_kdb_status.py
@@ -0,0 +1,38 @@
+import logging
+
+from django.core.management.base import BaseCommand
+from ...models import (  # isort:skip
+    Referral,
+)
+
+logger = logging.getLogger("partaj")
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for referral in Referral.objects.all():
+            if referral.default_send_to_knowledge_base is None:
+                # Get the default value depending on the unit
+                send_to_knowledge_base = False
+                for unit in referral.units.all():
+                    if unit.kdb_export == True:
+                        send_to_knowledge_base = True
+
+                # We set the default send status to the current selected defaults for the units
+                referral.default_send_to_knowledge_base = send_to_knowledge_base
+
+                # Case when a referral has a note in knowledge base but shouldn't have one by default
+                if referral.note and send_to_knowledge_base == False:
+                    # If the default value is False but the note exist, then we set the override value
+                    referral.override_send_to_knowledge_base = True
+                # Case when it's an old referral that wasn't sent to the knowledge base, but by default it should
+                elif (
+                    not referral.note
+                    and referral.report
+                    and referral.report.published_at
+                    and send_to_knowledge_base == True
+                ):
+                    # If the default value is True but the note doesn't exist, then we set the override value
+                    referral.override_send_to_knowledge_base = False
+
+                referral.save()
