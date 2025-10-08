e721bcf55279533ee771abe1404bb89824915092
Thomas Roger Lux <thomas.roger-lux@developpement-durable.gouv.fr>
2025-10-08 14:09:42 +0200

    Add translations

--- src/frontend/js/components/ReferralDetail/Header/UpdateKnowledgeBaseStateModal.tsx
+++ src/frontend/js/components/ReferralDetail/Header/UpdateKnowledgeBaseStateModal.tsx
@@ -2,6 +2,54 @@ import React from 'react';
 
 import { Referral } from 'types';
 import { ModalContainer, ModalSize } from '../../modals/ModalContainer';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+const messages = defineMessages({
+  addReferralTitle: {
+    defaultMessage: 'Add this referral to the knowledge base',
+    description:
+      'Title for the modal to add a published referral to the knowledge base',
+    id:
+      'components.ReferralDetail.UpdateKnowledgeBaseStateModal.addReferralTitle',
+  },
+  removeReferralTitle: {
+    defaultMessage: 'Remove this referral from the knowledge base',
+    description:
+      'Title for the modal to remove a published referral from the knowledge base',
+    id:
+      'components.ReferralDetail.UpdateKnowledgeBaseStateModal.removeReferralTitle',
+  },
+  addReferralDescription: {
+    defaultMessage:
+      'This referral will be added to the knowledge base, are you sure? This action will take effect by tomorrow.',
+    description:
+      'Description for the modal to add a published referral to the knowledge base',
+    id:
+      'components.ReferralDetail.UpdateKnowledgeBaseStateModal.addReferralDescription',
+  },
+  removeReferralDescription: {
+    defaultMessage:
+      'This referral will be removed from the knowledge base, are you sure? This action will take effect by tomorrow.',
+    description:
+      'Description for the modal to remove a published referral from the knowledge base',
+    id:
+      'components.ReferralDetail.UpdateKnowledgeBaseStateModal.removeReferralDescription',
+  },
+  addReferralButton: {
+    defaultMessage: 'Send to knowledge base',
+    description:
+      'Button for the modal to add a published referral to the knowledge base',
+    id:
+      'components.ReferralDetail.UpdateKnowledgeBaseStateModal.addReferralButton',
+  },
+  removeReferralButton: {
+    defaultMessage: 'Remove from knowledge base',
+    description:
+      'Button for the modal to remove a published referral from the knowledge base',
+    id:
+      'components.ReferralDetail.UpdateKnowledgeBaseStateModal.removeReferralButton',
+  },
+});
 
 interface UpdateKnowledgeBaseStateModalProps {
   isOpen: boolean;
@@ -30,14 +78,18 @@ export const UpdateKnowledgeBaseStateModal: React.FC<UpdateKnowledgeBaseStateMod
     >
       <div className="p-8 space-y-4">
         <h2 className="text-xl">
-          {sendToKnowledgeBase
-            ? 'Envoyer une saisine en base de connaissance'
-            : 'Retirer une saisine de la base de connaissance'}
+          {sendToKnowledgeBase ? (
+            <FormattedMessage {...messages.addReferralTitle} />
+          ) : (
+            <FormattedMessage {...messages.removeReferralTitle} />
+          )}
         </h2>
         <p className="text-x2 whitespace-pre-line">
-          {sendToKnowledgeBase
-            ? "Vous êtes sur le point d'envoyer cette saisine en base de connaissance, êtes vous sûr de votre choix ? Cette action sera effective à partir de demain."
-            : 'Vous êtes sur le point de retirer cette saisine de la base de connaissance, êtes vous sûr de votre choix ? Cette action sera effective à partir de demain.'}
+          {sendToKnowledgeBase ? (
+            <FormattedMessage {...messages.addReferralDescription} />
+          ) : (
+            <FormattedMessage {...messages.removeReferralDescription} />
+          )}
         </p>
       </div>
       <div className="flex justify-end bg-gray-300 p-8 space-x-4">
@@ -49,7 +101,11 @@ export const UpdateKnowledgeBaseStateModal: React.FC<UpdateKnowledgeBaseStateMod
           }}
           className="relative btn btn-primary"
         >
-          {sendToKnowledgeBase ? 'Envoyer' : 'Retirer'}
+          {sendToKnowledgeBase ? (
+            <FormattedMessage {...messages.addReferralButton} />
+          ) : (
+            <FormattedMessage {...messages.removeReferralButton} />
+          )}
         </button>
       </div>
     </ModalContainer>
