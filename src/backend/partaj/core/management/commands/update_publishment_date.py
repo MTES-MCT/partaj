1ee5506804de83ecb249fda24bd6f6a06264a9c9
AdJez <adrien.jezegou@i-carre.net>
2025-11-07 17:54:22 +0100

    Add command to fill publishment date and fix regular behavior when referral sent

---
+++ src/backend/partaj/core/management/commands/update_publishment_date.py
@@ -0,0 +1,51 @@
+"""
+Update ReferralNote states
+"""
+import logging
+
+from django.core.management.base import BaseCommand
+
+from partaj.core.models import Referral
+
+logger = logging.getLogger("partaj")
+
+
+class Command(BaseCommand):
+    """
+    Update report publishment date to last publishment
+    creation if exists
+    """
+
+    help = __doc__
+
+    def add_arguments(self, parser):
+        """
+        Define arguments
+        """
+        parser.add_argument("from", type=int)
+        parser.add_argument("to", type=int)
+
+    # pylint: disable=broad-except
+    def handle(self, *args, **options):
+        """
+        Create ReferralReportPublishment from Report Final Version
+        """
+        logger.info("Start updating publishment dates")
+
+        for referral in Referral.objects.filter(
+            id__gte=options["from"], id__lte=options["to"]
+        ).all():
+            logger.info("Handling referral n° %s", referral.id)
+
+            if (
+                len(referral.report.publishments.all()) > 0
+                and not referral.report.published_at
+            ):
+                logger.info(
+                    "Referral n° %s has a publishment, update its publishment date",
+                    referral.id,
+                )
+                publishment_date = referral.report.get_last_publishment().created_at
+
+                referral.report.published_at = publishment_date
+                referral.report.save()
