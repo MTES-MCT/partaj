54a465b4cca5b0d9732ce33158b86a383481eb34
abass-cgi <abass.ndiaye@cgi.com>
2026-01-14 12:17:22 +0100

    Fix pagination dashbord

--- src/frontend/js/utils/array.ts
+++
@@ -1,9 +0,0 @@
-export const getLastItem = (array: Array<any>) => {
-  return array.length > 0 ? array[array.length - 1] : null;
-};
-
-export const isInArray = (identifier: string, array: Array<string>) => {
-  const result = array.filter((item) => item === identifier);
-
-  return result.length > 0;
-};
