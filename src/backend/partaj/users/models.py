929c0db0dc57936351e12ee75862ef4b29fe04d3
AdJez <adrien.jezegou@i-carre.net>
2026-02-03 09:57:03 +0100

    Fix pylint

--- src/backend/partaj/users/models.py
+++ src/backend/partaj/users/models.py
@@ -10,6 +10,7 @@ from django.utils.translation import gettext_lazy as _
 
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """
     Partaj users are expected to authenticate themselves through Cerbère, an identity
