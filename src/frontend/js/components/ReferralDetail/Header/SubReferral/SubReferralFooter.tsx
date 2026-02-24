6c80a9c1aa08703cb09ad2f7028749e87957a04e
AdJez <adrien.jezegou@i-carre.net>
2026-02-24 14:04:58 +0100

    remove confirm split modal and save on network pb

--- src/frontend/js/components/ReferralDetail/Header/SubReferral/SubReferralFooter.tsx
+++ src/frontend/js/components/ReferralDetail/Header/SubReferral/SubReferralFooter.tsx
@@ -3,51 +3,23 @@ import { Referral } from '../../../../types';
 import { useSubReferral } from '../../../../data/providers/SubReferralProvider';
 import { CancelSplitButton } from '../../../buttons/CancelSplitButton';
 import { ConfirmSplitButton } from '../../../buttons/ConfirmSplitButton';
-import { ApiModalContext } from '../../../../data/providers/ApiModalProvider';
 import { ReferralContext } from '../../../../data/providers/ReferralProvider';
-import { useTranslateSubReferralField } from '../../../NewDashboard/utils';
 
 export const SubReferralFooter: ({
   referral,
 }: {
   referral: Referral;
 }) => JSX.Element = ({ referral }: { referral: Referral }) => {
-  const { getChangedFields } = useSubReferral();
-  const translateSubReferralField = useTranslateSubReferralField();
+  const { subFormState } = useSubReferral();
   const { setReferral } = useContext(ReferralContext);
-  const { openApiModal, closeApiModal } = useContext(ApiModalContext);
 
   return (
     <div className="w-full flex items-center justify-between pt-8">
       <CancelSplitButton referral={referral} />
       <ConfirmSplitButton
         referralId={referral.id}
-        beforeSplit={() => {
-          const changedFields = getChangedFields();
-          if (changedFields.length === 0) {
-            return true;
-          }
-          openApiModal({
-            title: "Confirmation de l'envoi",
-            content: () =>
-              `Le(s) champ(s)${changedFields.map(
-                (field) => ' ' + translateSubReferralField(field),
-              )} n'a / n'ont pas été enregistré(s). Souhaitez-vous vraiment valider cette sous-saisine sans les modifications apportées ?`,
-            button: (
-              <ConfirmSplitButton
-                referralId={referral.id}
-                beforeSplit={() => {
-                  return true;
-                }}
-                onSuccess={(referral: Referral) => {
-                  setReferral(referral);
-                  closeApiModal();
-                }}
-              />
-            ),
-            type: 'warning',
-          });
-        }}
+        subTitle={subFormState['sub_title'].currentValue}
+        subQuestion={subFormState['sub_question'].currentValue}
         onSuccess={(referral: Referral) => {
           setReferral(referral);
         }}
